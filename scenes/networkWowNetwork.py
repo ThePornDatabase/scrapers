b8a1643f967ab902c3cfd1ee6f7ace4d63eb5b4f
ChalupaBatman69 <admin@metadataapi.net>
2021-05-02 12:05:11 +1000

    couple wow girls fixes

--- scenes/networkWowNetwork.py
+++ scenes/networkWowNetwork.py
@@ -1,6 +1,3 @@
-import re
-
-import dateparser
 import scrapy
 
 from tpdb.BaseSceneScraper import BaseSceneScraper
@@ -8,7 +5,7 @@ from tpdb.BaseSceneScraper import BaseSceneScraper
 
 class VnaNetworkSpider(BaseSceneScraper):
     name = 'WowNetwork'
-    network = 'Wow'
+    network = 'Wow Girls'
 
     start_urls = [
         'https://www.wowpornblog.com/',
@@ -23,7 +20,7 @@ class VnaNetworkSpider(BaseSceneScraper):
         'image': '//meta[@property="og:image"]/@content',
         'performers': '//div[@itemprop="actor"]/ul/li/a/text()',
         'tags': '//div[@itemprop="keywords"]/ul/li/a/text()',
-        'external_id': '',                                          ### absolute no clue from regex
+        'external_id': '/([a-z0-9-]+?)/?$',
         'trailer': '//div[@class="video-embed"]/div/@data-item',
         'pagination': '/category/movies/page/%s/?filtre=date'
     }
@@ -34,25 +31,21 @@ class VnaNetworkSpider(BaseSceneScraper):
         scenes = response.css('.listing-videos li a::attr(href)').getall()
         for scene in scenes:
             yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
-    
+
     def get_site(self, response):
         site = response.xpath('//span[@itemprop="name"]/text()').extract_first()
         return site
 
-    def get_id(self, response):
-        external_id = response.url[:-1].rsplit('/',1)[1]
-        return external_id
-    
     def get_tags(self, response):
         if self.get_selector_map('tags'):
             tags = self.process_xpath(
                 response, self.get_selector_map('tags')).getall()
-            return list(map(lambda x: x.replace(' Movies','').strip(), tags))
+            return list(map(lambda x: x.replace(' Movies', '').strip(), tags))
         return []
-    
+
     def get_trailer(self, response):
         if 'trailer' in self.get_selector_map() and self.get_selector_map('trailer'):
             return self.process_xpath(
-                response, self.get_selector_map('trailer')).get().split('src":"',1)[1].split('","type',1)[0].replace('\\','').strip()
+                response, self.get_selector_map('trailer')).get().split('src":"', 1)[1].split('","type', 1)[0].replace(
+                '\\', '').strip()
         return ''
-
