c2d85f1b9bd27fe9351855829473c9f67e4ec7bf
Weslocke <briadin@yahoo.com>
2021-05-08 00:27:08 -0400

    initial commit

---
+++ scenes/sitePrivate.py
@@ -0,0 +1,40 @@
+import re
+
+import dateparser
+import scrapy
+
+from tpdb.BaseSceneScraper import BaseSceneScraper
+
+
+class PrivateSpider(BaseSceneScraper):
+    name = 'Private'
+    network = "Private"
+
+    start_urls = [
+        'https://www.private.com'
+    ]
+
+    selector_map = {
+        'title': '//meta[@itemprop="name"]/@content',
+        'description': '//meta[@itemprop="description"]/@content',
+        'date': '//meta[@itemprop="uploadDate"]/@content',
+        'image': '//meta[@itemprop="thumbnailUrl"]/@content',
+        'performers': '//ul[@class="scene-models-list"]/li/a[@data-track="PORNSTAR_NAME"]/text()',
+        'tags': '//ul[@class="scene-tags"]/li/a/text()',
+        'external_id': '\/(\d+)$',
+        'trailer': '',
+        'pagination': '/scenes/%s/'
+    }
+
+    def get_scenes(self, response):
+        scenes = response.xpath('//div[@class="scene"]/a/@href').getall()
+        for scene in scenes:
+            if re.search(self.get_selector_map('external_id'), scene):
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+
+    def get_site(self, response):
+        site = response.xpath('//span[@class="title-site"]/text()').get()
+        if site:
+            return site.strip()
+        else:
+            return "Private"
