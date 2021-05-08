7c0b325ee6bf68cf5f96bfec9a2eeb0edea5ddab
Weslocke <briadin@yahoo.com>
2021-05-08 02:19:23 -0400

    initial commit

---
+++ scenes/siteProducersFun.py
@@ -0,0 +1,68 @@
+import re
+
+import dateparser
+import scrapy
+
+from tpdb.BaseSceneScraper import BaseSceneScraper
+
+class ProducersFunSpider(BaseSceneScraper):
+    name = 'ProducersFun'
+    network = "Producers Fun"
+    parent = "Producers Fun"
+
+    start_urls = [
+        'https://producersfun.com'
+    ]
+
+    selector_map = {
+        'title': '//div[@class="shadow video-details"]/h1/text()',
+        'description': '(//div[@class="shadow video-details"]/p[not(@class)])[1]/text()',
+        'date': '//p[@class="video-date"]/text()[2]',
+        'image': '//section[@class="top-wrapper"]/div//video/@poster',
+        'performers': '//h1/text()',
+        'tags': '//p[@class="video-tags"]/a/text()',
+        'external_id': 'video\/(.*)',
+        'trailer': '',
+        'pagination': '/videos?page=%s'
+    }
+
+    def get_scenes(self, response):
+        scenes = response.xpath('//article[@class="shadow video"]/a/@href').getall()
+        for scene in scenes:
+            if re.search(self.get_selector_map('external_id'), scene):
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+
+    def get_site(self, response):
+        return "Producers Fun"
+
+    def get_trailer(self, response):
+        trailer = response.xpath('//input[@type="hidden" and @id="videoJsConfig"]/@value').get()
+        try:
+            trailer = re.search('480\},\{\"src\":\"(.*?)\".\"type', trailer).group(1)
+        except:
+            trailer = re.search('1080\},\{\"src\":\"(.*?)\".\"type', trailer).group(1)
+            
+        trailer = trailer.replace("\\","")
+        
+        if trailer:
+            return trailer
+        else:
+            return ''
+
+    def get_description(self, response):
+        description = response.xpath('//div[@class="shadow video-details"]/p[not(@class="video-date") and not(@class="video-tags")][1]/text()').get()
+        if description:
+            return description.strip()
+        else:
+            return ''
+            
+    def get_performers(self, response):
+        performers = response.xpath('//h1/text()').get()
+        if " - " in performers:
+            performers = re.search('(.*)\ -\ ', performers).group(1)
+            performers = performers.strip()
+            
+        if performers and "Volume" not in performers and "Compilation" not in performers:
+            return performers
+        else:
+            return ''
