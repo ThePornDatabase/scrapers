2c5cd80375c5a44437bb11d6b05a71829658ef53
Weslocke <briadin@yahoo.com>
2021-05-19 10:31:49 -0400

    additional tweaks for PornWorld

--- scenes/networkPornWorld.py
+++ scenes/networkPornWorld.py
@@ -21,8 +21,8 @@ class PornWorldScraper(BaseSceneScraper):
     ]
 
     selector_map = {
-        'title': "//meta[@itemprop='name']/@content",
-        'description': "//meta[@itemprop='description']/@content",
+        'title': "//div[@id='video-specs']/h1/text()",
+        'description': "//div[@class='descr-box']//p/text()",
         'date': "//meta[@itemprop='uploadDate']/@content",
         'image': '//meta[@itemprop="thumbnailUrl"]/@content',
         'performers': "//div[contains(@class,'pornstar-card')]//meta[@itemprop='name']/@content",
@@ -36,6 +36,8 @@ class PornWorldScraper(BaseSceneScraper):
         scenes = response.xpath(
             "//*[@id='scenesAjaxReplace']//a/@href").getall()
         for scene in scenes:
+            if re.match('.*\d{4}-\d{2}-\d{2}$', scene):
+                scene = re.search('(.*)\d{4}-\d{2}-\d{2}$', scene).group(1)
             if re.search(self.get_selector_map('external_id'), scene):
                 yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
 
@@ -48,3 +50,38 @@ class PornWorldScraper(BaseSceneScraper):
             date = response.xpath('//div[@id="video-specs"]/div/div/div[contains(@class, "d-inline-flex")]/p/text()').get()
             if re.match('\d{2}.\d{2}.\d{4}', date):
                 return dateparser.parse(date.strip()).isoformat()
+
+
+    def get_description(self, response):
+        
+        title = self.process_xpath(
+            response, self.get_selector_map('title')).get().strip()
+        if not title:
+            title = response.xpath('//meta[@itemprop="name"]/@content').group(1).strip()            
+
+        description = self.process_xpath(
+            response, self.get_selector_map('description')).get()
+
+        if not description:
+            description = response.xpath('//meta[@itemprop="description"]/@content').get()
+            
+        if not description:
+            description = title
+            
+        return description.replace("\\","").strip()
+        
+        
+    def get_title(self, response):
+        title = self.process_xpath(
+            response, self.get_selector_map('title')).get()
+        if not title:
+            title = response.xpath('//meta[@itemprop="name"]/@content').group(1).strip()
+        return title
+
+    def get_image(self, response):
+        image = self.process_xpath(response, self.get_selector_map('image')).get()
+        if not image:
+            image = response.xpath('//div[@class="video-box"]/div/img/@src').get()
+        if image:
+            image = image.strip()
+        return self.format_link(response, image)
