4fb024e689f8e14a2d97abd1ca110d10379a2902
Weslocke <briadin@yahoo.com>
2021-05-19 11:03:32 -0400

    adding performers to alternate PW sites

--- scenes/networkPornWorldAlt.py
+++ scenes/networkPornWorldAlt.py
@@ -34,14 +34,21 @@ class PornWorldScraper(BaseSceneScraper):
     }
 
     def get_scenes(self, response):
-        scenes = response.xpath("//div[@class='videoPic']/a/@href").getall()
+        scenes = response.xpath('//div[contains(@class,"videoBlock")]')
         for scene in scenes:
+            performerlist = scene.xpath('./div/div[@class="featuring"]/a/text()').getall()
+            if performerlist:
+                performers = [j.strip() for j in performerlist] 
+            scene = scene.xpath('./div[@class="videoPic"]/a/@href').get()
             if re.search(self.get_selector_map('external_id'), scene):
-                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+                if performers:
+                    yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene, meta={'performers': performers})
+                else:
+                    yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
 
     def get_date(self, response):
         search = re.search('Added:\\ (.*?\\d{2,4})\\ {1,3}', response.text)
         return dateparser.parse(search.group(1)).isoformat()
 
     def get_performers(self, response):
-        return []
+        return ["No Performers Available"]
