6c8a5dfd69d49b4cd6e818ab96252fb78bcbcee7
Weslocke <briadin@yahoo.com>
2021-06-02 05:52:10 -0400

    More fixes for TeamSkeet
    Ok, actually fixing it.  It now paginates for all sites, as well as fixing the urls for the individual scenes instead of pointing the the json page.  Tags are working, however dates are still missing on premium sites

--- scenes/networkTeamSkeet.py
+++ scenes/networkTeamSkeet.py
@@ -12,6 +12,7 @@ movies_nav_text = "newestMovies"
 videos_nav_text = "latestVideos"
 movies_content_text = "moviesContent"
 videos_content_text = "videosContent"
+v2_videos_content_text = "videoscontent"
 
 link_to_info = {
     "SLM-organic-b75inmn9fu": {
@@ -24,10 +25,11 @@ link_to_info = {
         "navText": movies_nav_text,
         "contentText": movies_content_text
     },
-    "ts-organic-iiiokv9kyo": {
+    "ts-elastic-d5cat0jl5o": {
         "site": "Team Skeet",
-        "navText": videos_nav_text,
-        "contentText": videos_content_text
+        "navText": v2_videos_content_text,
+        "contentText": v2_videos_content_text,
+        "v2": True
     },
     "mylf-organic-2uxkybtwvv": {
         "site": "MYLF",
@@ -122,24 +124,32 @@ link_to_info = {
 }
 
 
-def format_nav_url(link, start, limit):
-    nav_format = "https://store.psmcdn.net/{link}/{navText}/items.json?orderBy=\"$key\"&startAt=\"{start}\"&limitToFirst={limit}"
-    return nav_format.format(link=link, start=start,
-                             limit=limit, navText=link_to_info[link]["navText"])
+def format_nav_url(link, start, limit, v2=False):
+    if v2:
+        nav_format = "https://store2.psmcdn.net/{link}-{navText}/_search?from={start}&size={limit}"
+    else:
+        nav_format = "https://store.psmcdn.net/{link}/{navText}/items.json?orderBy=\"$key\"&startAt=\"{start}\"&limitToFirst={limit}"
+
+    return nav_format.format(link=link, start=start, limit=limit, navText=link_to_info[link]["navText"])
 
 
-def format_scene_url(link, sceneId):
-    if link == "ts-organic-iiiokv9kyo":
-        content_format = "https://store2.psmcdn.net/ts-elastic-d5cat0jl5o-videoscontent/_doc/{sceneId}"
+def format_scene_url(link, sceneId, v2=False):
+    if v2:
+        content_format = "https://store2.psmcdn.net/{link}-{contentText}/_doc/{sceneId}"
     else:
         content_format = "https://store.psmcdn.net/{link}/{contentText}/{sceneId}.json"
-    return content_format.format(
-        link=link, sceneId=sceneId, contentText=link_to_info[link]["contentText"])
+
+    return content_format.format(link=link, sceneId=sceneId, contentText=link_to_info[link]["contentText"])
 
 
-def get_site_link_text(url):
-    pattern = r"store.psmcdn.net\/([^\/]+)\/"
+def get_site_link_text(url, v2=False):
+    if v2:
+        pattern = r"store2.psmcdn.net\/([^\/]+)-.*\/"
+    else:
+        pattern = r"store.psmcdn.net\/([^\/]+)\/"
+
     site_link = re.search(pattern, url).groups()[0]
+
     return site_link
 
 
@@ -151,19 +161,52 @@ class TeamSkeetNetworkSpider(BaseSceneScraper):
         'external_id': '\\/(.+)\\.json'
     }
 
+            
     def start_requests(self):
-        start = "aaaaaaaa"
         limit = 250
+
         for linkName, siteInfo in link_to_info.items():
-            meta = {'site': siteInfo['site']}
-            yield scrapy.Request(url=format_nav_url(linkName, start, limit), callback=self.get_scenes, meta=meta)
+            if 'v2' in siteInfo:
+                is_v2 = siteInfo['v2']
+            else:
+                is_v2 = False
+            if is_v2:
+                start = "0"
+            else:
+                start = "aaaaaaaa"
+            url=format_nav_url(linkName, start, limit, is_v2)
+            yield scrapy.Request(url=format_nav_url(linkName, start, limit, is_v2),
+                                 callback=self.parse,
+                                 meta={'page': self.page, 'site': siteInfo['site']},
+                                 headers=self.headers,
+                                 cookies=self.cookies)
+
+    def parse(self, response, **kwargs):
+        limit = 250
+        scenes = self.get_scenes(response)
+        count = 0
+        for scene in scenes:
+            count += 1
+            yield scene
+
+        if count:
+            if 'page' in response.meta and response.meta['page'] < self.limit_pages:
+                meta = response.meta
+                meta['page'] = meta['page'] + 1
+                print('NEXT PAGE: ' + str(meta['page']))
+                yield scrapy.Request(url=self.get_next_page_url(response.url, meta['page']),
+                                     callback=self.parse,
+                                     meta=meta,
+                                     headers=self.headers,
+                                     cookies=self.cookies)            
 
     def parse_scene(self, response):
         data = response.json()
         item = SceneItem()
+        is_v2 = "store2" in response.url
+                
         if "store2" in response.url:
             data = data['_source']
-            
         item['title'] = data['title']
         item['description'] = data['description']
         item['image'] = data['img']
@@ -171,18 +214,16 @@ class TeamSkeetNetworkSpider(BaseSceneScraper):
             item['tags'] = data['tags']
         else:
             item['tags'] = []
-            
         item['id'] = data['id']
-        if 'video' in data:
+        
+        if 'videoTrailer' in data:
+            item['trailer'] = data['videoTrailer']
+        elif 'video' in data:
             item['trailer'] = 'https://videodelivery.net/' + \
                               data['video'] + '/manifest/video.m3u8'
         else:
             item['trailer'] = ''
-            
-        if "store2" in response.url:
-            item['url'] = "https://www.teamskeet.com/movies/" + data['id']
-        else:
-            item['url'] = response.url
+
         item['network'] = self.network
         item['parent'] = response.meta['site']
 
@@ -199,6 +240,12 @@ class TeamSkeetNetworkSpider(BaseSceneScraper):
         else:
             item['site'] = response.meta['site']
 
+        if is_v2:
+            item['url'] = "https://www.teamskeet.com/movies/" + data['id']
+        else:
+            item['url'] = "https://www." + response.meta['site'].replace(" ", "").lower() + ".com/movies/" + data['id']
+
+
         item['performers'] = []
         if 'models' in data:
             for model in data['models']:
@@ -211,10 +258,32 @@ class TeamSkeetNetworkSpider(BaseSceneScraper):
 
     def get_scenes(self, response):
         scene_info = json.loads(response.body)
-        site_link = get_site_link_text(response.url)
+        is_v2 = "store2" in response.url
+        site_link = get_site_link_text(response.url, is_v2)
 
-        for key, scene in scene_info.items():
-            if "id" in scene:
-                scene_id = scene["id"]
-                scene_url = format_scene_url(site_link, scene_id)
-                yield scrapy.Request(url=scene_url, callback=self.parse_scene, meta=response.meta)
+        if is_v2:
+            for scene in scene_info['hits']['hits']:
+                scene = scene['_source']
+                if "id" in scene:
+                    scene_id = scene["id"]
+                    scene_url = format_scene_url(site_link, scene_id, is_v2)
+                    yield scrapy.Request(url=scene_url, callback=self.parse_scene, meta=response.meta)      
+        else:
+            if scene_info:
+                for key, scene in scene_info.items():
+                    if "id" in scene:
+                        scene_id = scene["id"]
+                        scene_url = format_scene_url(site_link, scene_id, is_v2)
+                        yield scrapy.Request(url=scene_url, callback=self.parse_scene, meta=response.meta)
+
+
+    def get_next_page_url(self, base, page):
+        limit = 250
+        if 'store2' in base:
+            start = str(250 * (page-1))
+            is_v2 = True
+        else:
+            is_v2 = False
+            start = 'aaaaabka'
+        linkName = get_site_link_text(base, is_v2)
+        return format_nav_url(linkName, start, limit, is_v2)
