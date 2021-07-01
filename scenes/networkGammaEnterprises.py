03cd1325314c78798c949f33f6a056a56de7bf95
Weslocke <briadin@yahoo.com>
2021-07-01 08:54:26 -0400

    Very minor tweak to Gamma.  Additional date location

--- scenes/networkGammaEnterprises.py
+++ scenes/networkGammaEnterprises.py
@@ -163,7 +163,7 @@ class GammaEnterprisesSpider(BaseSceneScraper):
         ##############################
         # Standalone Sites
         ##############################
-        'https://www.21naturals.com',
+        # ~ 'https://www.21naturals.com',
         'https://www.activeduty.com',
         'https://www.bigfatcreampie.com',
         'https://www.bskow.com',
@@ -312,7 +312,7 @@ class GammaEnterprisesSpider(BaseSceneScraper):
     def parse_scene(self, response):
         global json
         data = response.css('script:contains("dataLayer =")::text').get()
-        data2 = response.xpath('//script[contains(text(),\'ScenePlayerId = "player"\')]').get()
+        data2 = response.xpath('//script[contains(text(),\'ScenePlayerId = "player"\')]|//script[contains(text(),\'ScenePlayerId = "scenePlayer"\')]').get()
         data3 = json.loads(response.xpath('//script[@type="application/ld+json"]/text()').get())
         data3 = data3[0]
 
