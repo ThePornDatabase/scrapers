f3e7d61f021ed641bdfe382e851a5bc490ef740e
Weslocke <briadin@yahoo.com>
2021-07-09 19:11:39 -0400

    Fixes for AdultEmpireCash

--- scenes/networkAdultEmpireCash.py
+++ scenes/networkAdultEmpireCash.py
@@ -33,18 +33,45 @@ class AdultEmpireCashScraper(BaseSceneScraper):
     }
 
     def get_scenes(self, response):
-        scenes = response.css('.grid-item')
-        for scene in scenes:
-            link = scene.css('a.grid-item-title::attr(href)').get()
-            meta = {}
-            if scene.css('p>span::text').get():
-                text = scene.css('p>span::text').get().strip().split('|')
-                if len(text) == 2:
-                    meta['site'] = text[0].strip()
-            yield scrapy.Request(url=self.format_link(response, link), callback=self.parse_scene, meta=meta)
+        if "spankmonster" in response.url:
+            scenes = response.xpath('//div[@class="animated-screenshot-container"]/a/@href').getall()
+            for scene in scenes:
+                meta = {}
+                meta['site'] = "Spank Monster"
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene, meta=meta)
+        else:
+            scenes = response.css('.grid-item')
+            for scene in scenes:
+                link = scene.css('a.grid-item-title::attr(href)').get()
+                meta = {}
+                if scene.css('p>span::text').get():
+                    text = scene.css('p>span::text').get().strip().split('|')
+                    if len(text) == 2:
+                        meta['site'] = text[0].strip()
+                yield scrapy.Request(url=self.format_link(response, link), callback=self.parse_scene, meta=meta)
 
     def get_site(self, response):
         if 'jayspov' in response.url:
             return 'Jays POV'
 
         return response.xpath('//div[@class="studio"]//span[2]/text()').get().strip()
+
+
+    def get_next_page_url(self, base, page):
+        pagination = self.get_selector_map('pagination')
+        
+        if "mypervyfamily" in base:
+            pagination = "/my-pervy-family.html?page=%s&view=grid"
+        if "jayspov" in base:
+            pagination = "/jays-pov-updates.html?page=%s&hybridview=member"
+        if "lantinoguys" in base:
+            pagination = "/watch-newest-latino-guys-porn-clips-and-scenes.html?page=%s&hybridview=member"
+        if "cospimps" in base:
+            pagination = "/videos/videos_page=%s"
+        if "pmggirls" in base:
+            pagination = "/videos/videos_page=%s"
+        if "lethalhardcore" in base:
+            pagination = "/watch-lethal-hardcore-streaming-video-scenes.html?page=%s&hybridview=member"
+        if "spankmonster" in base:
+            pagination = "/watch-newest-spank-monster-clips-and-scenes.html?page=%s&hybridview=member"
+        return self.format_url(base, pagination % page)
