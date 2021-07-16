d4fadf4fd6b91dafd817f391c9fc07c24bced46c
Weslocke <briadin@yahoo.com>
2021-07-15 23:57:11 -0400

    Initial commit of Playboy Plus

---
+++ scenes/sitePlayboyPlus.py
@@ -0,0 +1,71 @@
+import re
+import scrapy
+import tldextract
+from tpdb.BaseSceneScraper import BaseSceneScraper
+
+class sitePlayboyPlusSpider(BaseSceneScraper):
+    name = 'PlayboyPlus'
+    network = "Playboy"
+    parent = "Playboy"
+    
+    headers =  {
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    
+    start_urls = [
+        'https://www.playboyplus.com',
+    ]
+
+    selector_map = {
+        'title': '//div[contains(@class,"headline-container")]/h1/text()',
+        'description': '//p[contains(@class,"description")]//text()',
+        'date': '//p[@class="date label"]/text()',
+        'date_formats': ['%B %d, %Y'],
+        'image': '//div[@class="imageContainer"]//picture/source[1]/@data-srcset',
+        'performers': '//p[@class="contributorName"]/a/text()',
+        'tags': '//div[contains(@class,"headline-container")]/a[contains(@class,"tag")]/text()',
+        'external_id': '.*\/(.*)$',
+        'trailer': '',
+        'pagination': '/updates?page=%s&order=most-recent'
+    }
+
+    def get_scenes(self, response):
+        
+        htmlcode = response.text
+        results = re.findall('cardLink.*?href=\\\\"(.*?)\\\\"', htmlcode)
+        for scene in results:
+            scene = "https://www.playboyplus.com" + scene.replace("\\","")
+            if re.search(self.get_selector_map('external_id'), scene):
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+
+    def get_tags(self, response):
+        if self.get_selector_map('tags'):
+            tags = self.process_xpath(response, self.get_selector_map('tags'))
+            if tags:
+                return list(map(lambda x: x.replace(",","").strip().title(), tags.getall()))
+        return []
+
+        
+    def get_site(self, response):
+        return "Playboy Plus"
+
+
+    def get_description(self, response):
+        description = response.xpath(self.get_selector_map('description')).getall()
+        if description:
+            description = " ".join(description)   
+            description = description.replace(" ... ","").replace("\n","").replace("  ","")
+            return description.strip()
+
+        return ''
+
+
+    def get_image(self, response):
+        image = self.process_xpath(response, self.get_selector_map('image'))
+        if image:
+            image = self.get_from_regex(image.get(), 're_image')
+            if image:
+                image = image.replace(" ","%20")
+                return self.format_link(response, image)
+
+        return None
