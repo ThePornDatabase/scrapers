bbe27c962f01579e9bbc191dbbab6e2d17ddb452
Weslocke <briadin@yahoo.com>
2021-07-20 17:16:33 -0400

    tweaks to MyLifeInMiami
    Still not working though

--- scenes/siteMyLifeInMiami.py
+++ scenes/siteMyLifeInMiami.py
@@ -10,6 +10,7 @@ import string
 from datetime import datetime
 from tpdb.items import SceneItem
 
+
 class siteMyLifeInMiamiSpider(BaseSceneScraper):
     name = 'MyLifeInMiami'
     network = "My Life In Miami"
@@ -18,7 +19,7 @@ class siteMyLifeInMiamiSpider(BaseSceneScraper):
     start_urls = [
         'https://www.mylifeinmiami.com',
     ]
-
+    
     selector_map = {
         'title': '',
         'description': '',
@@ -30,18 +31,35 @@ class siteMyLifeInMiamiSpider(BaseSceneScraper):
         'external_id': 'scene\/(\d+)\/',
         'pagination': '/home_page=%s'
     }
-    
-    headers = {
-        'splash': '1',
-        '__ax': 'cdcSh4XJ3QtT2o75dGTCW',
-    }
-    
+
     def start_requests(self):
+        
+        yield scrapy.Request("https://www.mylifeinmiami.com/videos", callback=self.start_requests_2, cookies={'splash':'1'})
+        
+    def start_requests_2(self, response):
+        meta = {}
+        
+        appscript = response.xpath('//script[contains(text(),"fox.createApplication")]/text()').get()
+        if appscript:
+            ah = re.search('\"ah\":\"(.*?)\"', appscript).group(1)
+            aet = re.search('\"aet\":(\d+?),', appscript).group(1)
+            if ah and aet:
+                print(f'ah: {ah}')
+                print(f'aet: {aet}')
+                token = ah[::-1] + "/" + str(aet)
+                print(f'Token: {token}')
+                
+        if not token:
+            quit()
+        else:
+            meta['token'] = token
+            
         for link in self.start_urls:
-            url = url=self.get_next_page_url(link, self.page)
+            url = url=self.get_next_page_url(link, self.page, meta['token'])
+            meta['page'] = self.page
             yield scrapy.Request(url,
                                  callback=self.parse,
-                                 meta={'page': self.page},
+                                 meta=meta,
                                  headers=self.headers,
                                  cookies=self.cookies)
 
@@ -57,27 +75,28 @@ class siteMyLifeInMiamiSpider(BaseSceneScraper):
                 meta = response.meta
                 meta['page'] = meta['page'] + 1
                 print('NEXT PAGE: ' + str(meta['page']))
-                yield scrapy.Request(url=self.get_next_page_url(response.url, meta['page']),
+                yield scrapy.Request(url=self.get_next_page_url(response.url, meta['page'], meta['token']),
                                      callback=self.parse,
                                      meta=meta,
                                      headers=self.headers,
                                      cookies=self.cookies)
 
-    def get_next_page_url(self, base, page):
+    def get_next_page_url(self, base, page, token):
         page = str((int(page)-1) * 10)
-        page_url = "https://mylifeinmiami.com/sapi/GYyADCCqcoe2R9z0Cge7dw/1626714741/event.last?_method=event.last&offset=%s&limit=10&metaFields[totalCount]=1&transitParameters[v1]=ykYa8ALmUD&transitParameters[v2]=ykYa8ALmUD&transitParameters[showOnHome]=true"
+        page_url = "https://mylifeinmiami.com/sapi/" + token + "/event.last?_method=event.last&offset=%s&limit=10&metaFields[totalCount]=1&transitParameters[v1]=ykYa8ALmUD&transitParameters[v2]=ykYa8ALmUD&transitParameters[showOnHome]=true"
         return self.format_url(base, page_url % page)
 
 
     def get_scenes(self, response):
+        meta = response.meta
         global json
         jsondata = json.loads(response.text)
         jsondata = jsondata['response']['collection']
         
         for scene in jsondata:
             scene_id = scene['_typedParams']['id']
-            scene_url = "https://mylifeinmiami.com/sapi/Qld8qiCJ5S9rIJLaJkBnFw/1626716371/content.load?_method=content.load&tz=-4&filter[id][fields][0]=id&filter[id][values][0]=%s&limit=1&transitParameters[v1]=ykYa8ALmUD&transitParameters[preset]=scene" % scene_id
-            yield scrapy.Request(scene_url, callback=self.parse_scene)
+            scene_url = "https://mylifeinmiami.com/sapi/" + meta['token'] + "/content.load?_method=content.load&tz=-4&filter[id][fields][0]=id&filter[id][values][0]=%s&limit=1&transitParameters[v1]=ykYa8ALmUD&transitParameters[preset]=scene" % scene_id
+            yield scrapy.Request(scene_url, callback=self.parse_scene, headers=self.headers, cookies=self.cookies, meta=meta)
 
 
     def parse_scene(self, response):
