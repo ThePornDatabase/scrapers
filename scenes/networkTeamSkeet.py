b8dba04dde1b99e36f9154b4c78a4f18bc6ed0e1
Weslocke <briadin@yahoo.com>
2021-07-25 13:04:30 -0400

    Adding Perv Doctor to TeamSkeet

--- scenes/networkTeamSkeet.py
+++ scenes/networkTeamSkeet.py
@@ -159,6 +159,12 @@ link_to_info = {
         "contentText": movies_content_text,
         "v2": False
     },
+    "organic-doc-utei5Mai": {
+        "site": "Perv Doctor",
+        "navText": movies_nav_text,
+        "contentText": movies_content_text,
+        "v2": False
+    },
 }
 
 
