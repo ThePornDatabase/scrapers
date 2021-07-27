b9f09da77713576e6bb205102c6ec23d9a98bc3d
Weslocke <briadin@yahoo.com>
2021-07-27 18:28:17 -0400

    Initial Commit of Cospuri and performers

---
+++ performers/siteCospuriPerformer.py
@@ -0,0 +1,118 @@
+import scrapy
+import re
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import dateparser
+from tpdb.BasePerformerScraper import BasePerformerScraper
+
+
+class siteCospuriPerformerSpider(BasePerformerScraper):
+    selector_map = {
+        'name': '//div[@class="name-en"]/text()',
+        'image': '//div[@class="model-costume"]/@style',
+        'bio': '//div[contains(@class,"hidden-xs")]/p/text()',
+        'nationality': '//div[@class="item country"]/span/text()',
+        'height': '//div[@class="item height"]/span/text()',
+        'country': '//div[@class="item country"]/span/text()',
+        'measurements': '//div[@class="item measurements"]/span/text()',
+        'pagination': '/models/page/%s',
+        'external_id': 'model\/(.*)/'
+    }
+
+    name = 'CospuriPerformer'
+    network = 'Cospuri'
+
+    start_urls = [
+        'https://www.cospuri.com',
+    ]
+
+
+    def start_requests(self):
+        url = "https://www.cospuri.com/models/"
+        for link in self.start_urls:
+            yield scrapy.Request(url,
+                                 callback=self.get_performers,
+                                 meta={'page': self.page},
+                                 headers=self.headers,
+                                 cookies=self.cookies)
+
+    def get_gender(self, response):
+        return 'Female'
+
+    def get_performers(self, response):
+        performers = response.xpath('//div[contains(@class,"model-thumb")]/a/@href').getall()
+        for performer in performers:
+            yield scrapy.Request(
+                url=self.format_link(response, performer),
+                callback=self.parse_performer
+            )
+
+    def get_measurements(self, response):
+        if 'measurements' in self.selector_map:
+            measurements = self.process_xpath(response, self.get_selector_map('measurements')).get()
+            if measurements and re.match('B\d{2,3}.*?W\d{2,3}.*?H\d{2,3}', measurements):
+                bust = re.search('B(\d{2,3})', measurements).group(1)
+                if bust:
+                    bust = round(int(bust)/2.54)
+                waist = re.search('W(\d{2,3})', measurements).group(1)
+                if waist:
+                    waist = round(int(waist)/2.54)
+                hips = re.search('H(\d{2,3})', measurements).group(1)
+                if hips:
+                    hips = round(int(hips)/2.54)
+                
+                cupsize = re.search('B\d+-([a-zA-Z]+)', measurements).group(1)
+                if cupsize:
+                    if cupsize == "N/A":
+                        cupsize = ''
+                    else:
+                        cupsize = cupsize.strip()
+
+                if bust and waist and hips and cupsize:
+                    measurements = str(bust) + cupsize + "-" + str(waist) + "-" + str(hips)
+                    return measurements.strip()
+                elif bust and waist and hips:
+                    measurements = str(bust) + "-" + str(waist) + "-" + str(hips)
+                    return measurements.strip()
+        return ''
+
+    def get_cupsize(self, response):
+        if 'measurements' in self.selector_map:
+            measurements = self.process_xpath(response, self.get_selector_map('measurements')).get()
+            if measurements and re.match('B\d{2,3}.*?W\d{2,3}.*?H\d{2,3}', measurements):
+                bust = re.search('B(\d{2,3})', measurements).group(1)
+                if bust:
+                    bust = round(int(bust)/2.54)
+
+                cupsize = re.search('B\d+-([a-zA-Z]+)', measurements).group(1)
+                if cupsize:
+                    if cupsize == "N/A":
+                        cupsize = ''
+                    else:
+                        cupsize = cupsize.strip()
+
+                if bust and cupsize:
+                    cupsize = str(bust) + cupsize
+                    return cupsize.strip()
+                elif cupsize:
+                    return cupsize.strip()
+        return ''
+
+    def get_image(self, response):
+        image = self.process_xpath(response, self.get_selector_map('image')).get()
+        if image:
+            image = re.search('(http.*\.jpg)', image).group(1)
+            if image:
+                return self.format_link(response, image.strip())
+        return ''
+
+    def get_height(self, response):
+        if 'height' in self.selector_map:
+            height = self.process_xpath(response, self.get_selector_map('height')).get()
+            if height:
+                if "cm" in height and re.match('(\d+\s?cm)', height):
+                    height = re.search('(\d+\s?cm)', height).group(1).strip()
+                    height = height.replace(" ","")
+                if "0 ft" not in height:
+                    return height.strip()
+        return ''
---
+++ scenes/siteCospuri.py
@@ -0,0 +1,94 @@
+import scrapy
+import re
+import dateparser
+from tpdb.BaseSceneScraper import BaseSceneScraper
+
+
+class siteCospuriSpider(BaseSceneScraper):
+    name = 'Cospuri'
+    network = 'Cospuri'
+    parent = 'Cospuri'
+
+    url = 'https://www.cospuri.com/'
+
+    paginations = [
+        '/samples?channel=cosplay&page=%s',
+        '/samples?channel=bukkake&page=%s',
+        '/samples?channel=schoolgirl&page=%s',
+        '/samples?channel=harajuku&page=%s',
+    ]
+
+    selector_map = {
+        'title': '//div[@class="sample-model"]/a/text()',
+        'description': '//div[@class="description"]/text()',
+        'date': '//div[@class="date"]/text()',
+        'image': '//div[@class="vid cosplay"]/div/@style',
+        're_image': '\((http.*.jpg)\)',
+        'performers': '//div[@class="sample-model"]/a/text()',
+        'tags': '//div[@class="tags"]/a/text()',
+        'external_id': 'id=(.*)',
+        'trailer': '//script[contains(text(),"sources")]/text()',
+        're_trailer': '(https.*\.mp4)',
+        'pagination': '/samples?channel=cosplay&page=%s'
+    }
+    
+
+    def start_requests(self):
+        for pagination in self.paginations:
+            yield scrapy.Request(url=self.get_next_page_url(self.url, self.page, pagination),
+                                 callback=self.parse,
+                                 meta={
+                'page': self.page, 'pagination': pagination},
+                headers=self.headers,
+                cookies=self.cookies)
+
+    def parse(self, response, **kwargs):
+        if response.status == 200:
+            scenes = self.get_scenes(response)
+            count = 0
+            for scene in scenes:
+                count += 1
+                yield scene
+
+            if count:
+                if 'page' in response.meta and response.meta['page'] < self.limit_pages:
+                    meta = response.meta
+                    meta['page'] = meta['page'] + 1
+                    print('NEXT PAGE: ' + str(meta['page']))
+                    yield scrapy.Request(url=self.get_next_page_url(self.url, meta['page'], meta['pagination']),
+                                         callback=self.parse,
+                                         meta=meta,
+                                         headers=self.headers,
+                                         cookies=self.cookies)
+
+    def get_next_page_url(self, url, page, pagination):
+        return self.format_url(url, pagination % page)    
+
+    def get_scenes(self, response):
+        scenes = response.xpath('//div[@class="scene cosplay"]/div/a/@href').getall()
+        for scene in scenes:
+            if re.search(self.get_selector_map('external_id'), scene):
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+
+    def get_site(self, response):
+        return "Cospuri"
+        
+
+    def get_parent(self, response):
+        return "Cospuri"
+
+
+    def get_date(self, response):
+        date = self.process_xpath(response, self.get_selector_map('date')).get()
+        if date:
+            # ~ print(f'Date: {date}')
+            dates = re.search('(\d{4}).*?(\d{2}).*?(\d{2})', date)
+            if dates:
+                year = dates.group(1)
+                month = dates.group(2)
+                day = dates.group(3)
+                date = year + month + day
+
+            return dateparser.parse(date, date_formats=['%Y%m%d']).isoformat()
+
+        return None
