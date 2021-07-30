8d259819e9222d5ed5c457df7e7b03f7355d9b30
Weslocke <briadin@yahoo.com>
2021-07-30 01:26:01 -0400

    initial commit of Monger in Asia and performers

---
+++ performers/siteMongerInAsiaPerformer.py
@@ -0,0 +1,60 @@
+import scrapy
+import re
+import html
+
+from tpdb.items import PerformerItem
+from tpdb.BasePerformerScraper import BasePerformerScraper
+
+
+class siteMongerInAsiaPerformerSpider(BasePerformerScraper):
+    selector_map = {
+        'pagination': '/categories/models_%s_d',
+        'external_id': 'models\/(.*).html'
+    }
+
+    name = 'MongerInAsiaPerformer'
+    network = "Monger In Asia"
+
+    start_urls = [
+        'https://www.mongerinasia.com'
+    ]
+
+    def get_performers(self, response):
+        performers = response.xpath('//div[@class="model"]')
+        for performer in performers:
+            item = PerformerItem()
+            
+            name = performer.xpath('./div/p/text()').get()
+            if name:
+                item['name'] = html.unescape(name.strip().title())
+            else:
+                item['name'] = ''
+
+            image = performer.xpath('.//img/@src').get()
+            if image:
+                item['image'] = image.strip()
+            else:
+                item['image'] = ''
+                
+            item['url'] = response.url
+                
+            item['network'] = 'Monger in Asia'
+            
+            item['astrology'] = ''
+            item['bio'] = ''
+            item['birthday'] = ''
+            item['birthplace'] = ''
+            item['cupsize'] = ''
+            item['ethnicity'] = ''
+            item['eyecolor'] = ''
+            item['fakeboobs'] = ''
+            item['gender'] = 'Female'
+            item['haircolor'] = ''
+            item['height'] = ''
+            item['measurements'] = ''
+            item['nationality'] = ''
+            item['piercings'] = ''
+            item['tattoos'] = ''
+            item['weight'] = ''                
+            
+            yield item            
