da8d5807dfeee41f063231550a19dfc9e3854300
jane558654 <82664900+jane558654@users.noreply.github.com>
2021-07-31 15:44:42 +0100

    Add files via upload

---
+++ scenes/siteDeepLush.py
@@ -0,0 +1,32 @@
+import scrapy
+from tpdb.BaseSceneScraper import BaseSceneScraper
+from tpdb.items import SceneItem
+
+class DeepLushSpider(BaseSceneScraper):
+    name = 'DeepLush'
+    network = 'Deep Lush'
+    parent = 'Deep Lush'
+
+    start_urls = ["https://deeplush.com"]
+
+    selector_map = {
+        'title': '//h2/text()',
+        'description': '//div[@class="collapse "]//text()',
+        'tags': '//a[contains(@href,"/video/category/")]//text()',
+        'performers': '//a[contains(@class,"performer")]//text()',
+        'image': '//video/@poster',
+        'trailer': '//video/source[last()]/@src',
+        'date': '//span[@class="date"]//text()',
+        'date_formats': ['%d %b %Y'],
+        'external_id': '[0-9]+/(.+)',
+        'pagination': 'video/gallery/%s',
+    }
+
+    def get_next_page_url(self, base, page):
+        return self.format_url(base, self.get_selector_map('pagination') % ((page-1) * 12))
+
+    def get_scenes(self, response):
+        for scene in response.xpath('//a[contains(@href, "/video/watch/")]/@href').getall():
+            yield scrapy.Request(
+                url=self.format_link(response, scene),
+                callback=self.parse_scene)
