502ef15a0e624e9414156f6a26270bf21848cf6b
DirtyRacer1337 <DirtyRacer@ya.ru>
2021-08-06 20:21:09 +0700

    Update networkSteppedUp.py

--- scenes/networkSteppedUp.py
+++ scenes/networkSteppedUp.py
@@ -18,28 +18,23 @@ class SteppedUpSpider(BaseSceneScraper):
     ]
 
     selector_map = {
-        'title': '//h1[@class="title"]/text() | //h2[@class="title"]/text()',
-        'description': '//div[contains(@class, "desc")]/p/text() | //div[contains(@class, "content-description")]/p/text()',
-        'date': "//div[@class='content-page-info']/div[@class='content-meta-wrap']/div[@class='content-meta']/p/span[@class='post-date']/text() | //div[@class='content-meta']//span[contains(@class,'date')]/text()",
-        'performers': "//div[@class='content-meta-wrap']/div[@class='content-meta']/h4[@class='models']/a/text()",
-        'tags': "",
+        'title': '//*[@class="title"]/text()',
+        'description': '//div[contains(@class, "desc")]/p/text()',
+        'date': '//div[@class="content-page-info"]//span[@class="post-date"]/text() | //div[@class="content-meta"]//span[contains(@class, "date")]/text()',
+        'performers': '//div[@class="content-meta-wrap"]//h4[@class="models"]/a/text()',
+        'tags': '',
         'trailer': '//video/source/@src',
-        'external_id': 'view/([^/]+)',
+        'external_id': r'view/([^/]+)',
         'pagination': '/videos?page=%s'
     }
 
     def get_scenes(self, response):
-        scenes = response.xpath(
-            '//div[contains(@class, "thumb-full")] | //div[@class="content-border"]')
+        scenes = response.xpath('//div[contains(@class, "thumb-full")] | //div[@class="content-border"]')
         for scene in scenes:
             link = scene.css('a::attr(href)').get()
-            meta = {
-                'image': scene.css('a img::attr(src)').get()
-            }
 
-            if re.search(self.get_selector_map(
-                    'external_id'), link) is not None:
-                yield scrapy.Request(url=self.format_link(response, link), callback=self.parse_scene, meta=meta)
+            if re.search(self.get_selector_map('external_id'), link) is not None:
+                yield scrapy.Request(url=self.format_link(response, link), callback=self.parse_scene)
 
     def get_image(self, response):
         xpaths = [
@@ -47,8 +42,14 @@ class SteppedUpSpider(BaseSceneScraper):
             '//video[contains(@id, "ypp-player")]/@poster',
             '//a[@href="%s"]//img/@src' % response.url,
             '//div[@class="view-thumbs"]//img/@src',
+            '//a//img[@src]/@src'
         ]
 
         for xpath in xpaths:
-            if response.xpath(xpath) is not None:
-                return response.xpath(xpath).get()
+            image = response.xpath(xpath)
+            if image is not None:
+                image = image.get()
+                if image is not None:
+                    return image
+
+        return None
