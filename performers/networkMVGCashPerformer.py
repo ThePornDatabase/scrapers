08dd87e6b389ba488fd299eeda5fc2797e816f3a
Weslocke <briadin@yahoo.com>
2021-09-01 13:51:08 -0400

    initial commit of MVG Cash sites and performers

---
+++ performers/networkMVGCashPerformer.py
@@ -0,0 +1,56 @@
+import scrapy
+import html
+
+from tpdb.BasePerformerScraper import BasePerformerScraper
+from tpdb.items import PerformerItem
+
+class siteAmericanPornstarSpider(BasePerformerScraper):
+    selector_map = {
+        'name': '//div[contains(@class,"model modelfeature")]//h3/a/text()',
+        'image': '//div[contains(@class,"model modelfeature")]/div/a/img/@src0_1x',
+        'pagination': '/models/%s/latest/',
+        'external_id': 'girls/(.+)/?$'
+    }
+    name = 'MVGCashPerformer'
+    network = "MVG Cash"
+    start_urls = [
+        'https://italianshotclub.com',
+        'https://lesbiantribe.com',
+        'https://myslutwifegoesblack.com',
+        'https://pornlandvideos.com',
+        'https://sologirlsmania.com',
+        'https://vangoren.com',
+    ]
+
+    def get_gender(self, response):
+        return 'Female'
+
+    def get_performers(self, response):
+        performers = response.xpath('//div[contains(@class,"model modelfeature")]')
+        for performer in performers:
+            item = PerformerItem()
+            name = performer.xpath('.//h3/a/text()').get()
+            if name:
+                item['name'] = html.unescape(name.strip().title())
+            item['image'] = performer.xpath('./div/a/img/@src0_1x').get().strip()
+            url = performer.xpath('.//h3/a/@href').get()
+            if url:
+                item['url'] = url.strip()
+            item['network'] = 'MVG Cash'
+            item['astrology'] = ''
+            item['bio'] = ''
+            item['birthday'] = ''
+            item['birthplace'] = ''
+            item['cupsize'] = ''
+            item['ethnicity'] = ''
+            item['eyecolor'] = ''
+            item['fakeboobs'] = ''
+            item['gender'] = ''
+            item['haircolor'] = ''
+            item['height'] = ''
+            item['measurements'] = ''
+            item['nationality'] = ''
+            item['piercings'] = ''
+            item['tattoos'] = ''
+            item['weight'] = ''                
+            yield item
