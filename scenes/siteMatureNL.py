40b0b90216570a5ef5005c48497d260e6440c190
Weslocke <briadin@yahoo.com>
2021-09-01 21:08:06 -0400

    changing file for Reviewdog

--- scenes/siteMatureNL.py
+++ scenes/siteMatureNL.py
@@ -3,16 +3,12 @@ import re
 import dateparser
 from tpdb.BaseSceneScraper import BaseSceneScraper
 
-
 class siteMatureNLSpider(BaseSceneScraper):
     name = 'MatureNL'
     network = 'Mature NL'
-
-
     start_urls = [
         'https://www.mature.nl',
     ]
-
     selector_map = {
         'title': '//h1/text()',
         'description': '//meta[@name="description"]/@content',
@@ -20,7 +16,9 @@ class siteMatureNLSpider(BaseSceneScraper):
         're_date': '(\d{1,2}-\d{1,2}-\d{4})',
         'date_formats': ['%d-%m-%Y'],
         'image': '//span[@id="spnPageUpdateTrailer"]//img/@data-src',
-        'performers': '//div[@class="box-cnt"]//div[@class="grid-tile-model"]/div[@class="name"]/span/text()',
+        'performers': 
+            '//div[@class="box-cnt"]//div[@class="grid-tile-model"]'\
+            '/div[@class="name"]/span/text()',
         'tags': '//div[@class="box-cnt"]//a[contains(@href, "/niche/")]/text()',
         'external_id': 'update\/(\d+)\/',
         'trailer': '//script[contains(text(),"showTrailer")]/text()',
@@ -38,18 +36,19 @@ class siteMatureNLSpider(BaseSceneScraper):
                     print(scene)
             if "upid=" in scene:
                 sceneid = re.search('upid=(\d+)', scene).group(1)
-            
+
             scene = "https://www.mature.nl/en/update/" + sceneid.strip()+ "/"
-            
+
             if re.search(self.get_selector_map('external_id'), scene):
-                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+                yield scrapy.Request(url=self.format_link(response, scene), 
+                        callback=self.parse_scene)
 
     def get_site(self, response):
         return "Mature NL"
 
     def get_parent(self, response):
         return "Mature NL"
-        
+
     def get_performers (self, response):
         performers = super().get_performers(response)
         if performers:
@@ -59,7 +58,6 @@ class siteMatureNLSpider(BaseSceneScraper):
                     performer = re.sub('\(.*?\)', '', performer)
                 performer = performer.strip()
                 performers[i] = performer
-                    
         return performers
         
     def get_image(self, response):
@@ -70,29 +68,30 @@ class siteMatureNLSpider(BaseSceneScraper):
                 image = image.get().strip()
             else:
                 image = ''
-                    
         return image
         
     def get_date(self, response):
         date = self.process_xpath(response, self.get_selector_map('date'))
         if date:
             date = date.get()
-            
         if not date or not re.search('(\d{1,2}-\d{1,2}-\d{4})', date):
-            date = response.xpath('//div[@class="container update-bg-container"]/div[@class="box"][1]/div[@class="box-cnt"]/div[1]')
+            date = response.xpath('//div[@class="container update-bg-container"]'\
+                    '/div[@class="box"][1]/div[@class="box-cnt"]/div[1]')
             if date:
                 date = date.get()
-                
         if date and re.search('(\d{1,2}-\d{1,2}-\d{4})', date):
             date = self.get_from_regex(date, 're_date')
-            date_formats = self.get_selector_map('date_formats') if 'date_formats' in self.get_selector_map() else None
-
+            if 'date_formats' in self.get_selector_map():
+                date_formats = self.get_selector_map('date_formats')
+            else:
+                None
             return dateparser.parse(date, date_formats=date_formats).isoformat()
             
     def get_title(self, response):
         title = super().get_title(response)
         if "watch this scene exclusively" in title.lower():
-            newtitle = response.xpath('//div[@class="box-cnt"]//div[@class="grid-tile-model"]/div[@class="name"]/span/text()')
+            newtitle = response.xpath('//div[@class="box-cnt"]'\
+                    '//div[@class="grid-tile-model"]/div[@class="name"]/span/text()')
             if newtitle:
                 newtitle = newtitle.getall()
                 newtitle = " and ".join(newtitle)
