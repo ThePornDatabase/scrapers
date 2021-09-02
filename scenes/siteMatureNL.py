96a3ef573b771888864c5668601f0990538931a4
Weslocke <briadin@yahoo.com>
2021-09-01 21:13:42 -0400

    Still tweaking for ReviewDog

--- scenes/siteMatureNL.py
+++ scenes/siteMatureNL.py
@@ -3,6 +3,7 @@ import re
 import dateparser
 from tpdb.BaseSceneScraper import BaseSceneScraper
 
+
 class siteMatureNLSpider(BaseSceneScraper):
     name = 'MatureNL'
     network = 'Mature NL'
@@ -10,28 +11,29 @@ class siteMatureNLSpider(BaseSceneScraper):
         'https://www.mature.nl',
     ]
     selector_map = {
-        'title': '//h1/text()',
-        'description': '//meta[@name="description"]/@content',
-        'date': '//div[@class="box-cnt"]/div[@class="mar-t"][1]/text()[1]',
-        're_date': '(\d{1,2}-\d{1,2}-\d{4})',
-        'date_formats': ['%d-%m-%Y'],
-        'image': '//span[@id="spnPageUpdateTrailer"]//img/@data-src',
+        'title': r'//h1/text()',
+        'description': r'//meta[@name="description"]/@content',
+        'date': r'//div[@class="box-cnt"]/div[@class="mar-t"][1]/text()[1]',
+        're_date': r'(\d{1,2}-\d{1,2}-\d{4})',
+        'date_formats': [r'%d-%m-%Y'],
+        'image': r'//span[@id="spnPageUpdateTrailer"]//img/@data-src',
         'performers': 
-            '//div[@class="box-cnt"]//div[@class="grid-tile-model"]'\
+            r'//div[@class="box-cnt"]//div[@class="grid-tile-model"]'\
             '/div[@class="name"]/span/text()',
-        'tags': '//div[@class="box-cnt"]//a[contains(@href, "/niche/")]/text()',
-        'external_id': 'update\/(\d+)\/',
-        'trailer': '//script[contains(text(),"showTrailer")]/text()',
-        're_trailer': '\"(http.*?\.mp4)\"',
-        'pagination': '/en/updates/%s'
+        'tags': r'//div[@class="box-cnt"]'\
+            '//a[contains(@href, "/niche/")]/text()',
+        'external_id': r'update\/(\d+)\/',
+        'trailer': r'//script[contains(text(),"showTrailer")]/text()',
+        're_trailer': r'\"(http.*?\.mp4)\"',
+        'pagination': r'/en/updates/%s'
     }
 
     def get_scenes(self, response):
-        scenes = response.xpath('//div[@class="grid-item"]/div/a/@href').getall()
+        scenes = response.xpath(r'//div[@class="grid-item"]/div/a/@href').getall()
         for scene in scenes:
             if "/update/" in scene:
                 try:
-                    sceneid = re.search('\/update\/(\d+)', scene).group(1)
+                    sceneid = re.search(r'\/update\/(\d+)', scene).group(1)
                 except:
                     print(scene)
             if "upid=" in scene:
@@ -54,8 +56,8 @@ class siteMatureNLSpider(BaseSceneScraper):
         if performers:
             for i in range(len(performers)):
                 performer = performers[i]
-                if re.search('.*\(.*?\)', performer):
-                    performer = re.sub('\(.*?\)', '', performer)
+                if re.search(r'.*\(.*?\)', performer):
+                    performer = re.sub(r'\(.*?\)', '', performer)
                 performer = performer.strip()
                 performers[i] = performer
         return performers
@@ -63,7 +65,9 @@ class siteMatureNLSpider(BaseSceneScraper):
     def get_image(self, response):
         image = super().get_image(response)
         if not image:
-            image = response.xpath('//div[@class="box-cnt"]//a[@class="mfp-image"]/@href')
+            image = response.xpath(
+                r'//div[@class="box-cnt"]//a[@class="mfp-image"]/@href'
+            )
             if image:
                 image = image.get().strip()
             else:
@@ -74,12 +78,13 @@ class siteMatureNLSpider(BaseSceneScraper):
         date = self.process_xpath(response, self.get_selector_map('date'))
         if date:
             date = date.get()
-        if not date or not re.search('(\d{1,2}-\d{1,2}-\d{4})', date):
-            date = response.xpath('//div[@class="container update-bg-container"]'\
+        if not date or not re.search(r'(\d{1,2}-\d{1,2}-\d{4})', date):
+            date = response.xpath(
+                    r'//div[@class="container update-bg-container"]'\
                     '/div[@class="box"][1]/div[@class="box-cnt"]/div[1]')
             if date:
                 date = date.get()
-        if date and re.search('(\d{1,2}-\d{1,2}-\d{4})', date):
+        if date and re.search(r'(\d{1,2}-\d{1,2}-\d{4})', date):
             date = self.get_from_regex(date, 're_date')
             if 'date_formats' in self.get_selector_map():
                 date_formats = self.get_selector_map('date_formats')
@@ -90,7 +95,7 @@ class siteMatureNLSpider(BaseSceneScraper):
     def get_title(self, response):
         title = super().get_title(response)
         if "watch this scene exclusively" in title.lower():
-            newtitle = response.xpath('//div[@class="box-cnt"]'\
+            newtitle = response.xpath(r'//div[@class="box-cnt"]'\
                     '//div[@class="grid-tile-model"]/div[@class="name"]/span/text()')
             if newtitle:
                 newtitle = newtitle.getall()
