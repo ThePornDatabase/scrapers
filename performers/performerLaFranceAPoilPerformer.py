34e61f47adf5a6be0e7cace7df361c9e55226b28
Weslocke <briadin@yahoo.com>
2021-09-05 15:33:21 -0400

    Initial commit of La France a Poil

---
+++ performers/performerLaFranceAPoilPerformer.py
@@ -0,0 +1,75 @@
+import html
+from unidecode import unidecode
+from scrapy import Selector
+
+from tpdb.BasePerformerScraper import BasePerformerScraper
+from tpdb.items import PerformerItem
+
+
+class SiteLaFranceAPoilPerformerSpider(BasePerformerScraper):
+    selector_map = {
+        'name': '//div[contains(@class,"thumbstar")]//div[@class="pull-left"]/text()',
+        'image': '//div[contains(@class,"thumbstar")]//img/@src',
+        'pagination': '/portal/morestars.php?aff=&page=%s&tag=&video=&thid=&tr=&trlfap=&cp=&tunl=&iduser=&sort=&lang=en',
+        'external_id': 'girls/(.+)/?$'
+    }
+
+    name = 'LaFranceAPoilPerformer'
+    network = 'La France a Poil'
+
+    start_urls = [
+        'https://www.lafranceapoil.com',
+    ]
+
+    cookies = {
+        'disclaimerlfap': 'oui',
+    }
+
+    def get_gender(self, response):
+        return 'Female'
+
+    def get_performers(self, response):
+        response_text = response.text
+        response_text = response_text.replace("\\", "")
+        response_xpath = Selector(text=response_text)
+
+        performers = response_xpath.xpath('//a[contains(@href,"amatrice=")]')
+        for performer in performers:
+            item = PerformerItem()
+
+            name = performer.xpath('.//div[@class="pull-left"]/text()').get()
+            if name:
+                name = unidecode(name)
+                item['name'] = html.unescape(name.strip().title())
+
+            image = performer.xpath('.//img/@src').get()
+            if image:
+                item['image'] = image.strip()
+
+            url = performer.xpath('./@href').get()
+            if url:
+                item['url'] = "https://www.lafranceapoil.com/en/" + url.strip()
+
+            item['network'] = 'La France a Poil'
+
+            item['height'] = ''
+            item['weight'] = ''
+            item['astrology'] = ''
+            item['bio'] = ''
+            item['birthday'] = ''
+            item['birthplace'] = ''
+            item['cupsize'] = ''
+            item['ethnicity'] = ''
+            item['eyecolor'] = ''
+            item['fakeboobs'] = ''
+            item['gender'] = 'Female'
+            item['haircolor'] = ''
+            item['measurements'] = ''
+            item['nationality'] = ''
+            item['piercings'] = ''
+            item['tattoos'] = ''
+            yield item
+
+    def get_next_page_url(self, base, page):
+        page = str(int(page) - 1)
+        return self.format_url(base, self.get_selector_map('pagination') % page)
---
+++ scenes/siteLaFranceAPoil.py
@@ -0,0 +1,68 @@
+import re
+import html
+from unidecode import unidecode
+import scrapy
+
+from tpdb.BaseSceneScraper import BaseSceneScraper
+
+
+class SiteLaFranceAPoilSpider(BaseSceneScraper):
+    name = 'LaFranceAPoil'
+    network = 'La France a Poil'
+
+    start_urls = [
+        'https://www.lafranceapoil.com',
+    ]
+
+    cookies = {
+        'disclaimerlfap': 'oui',
+    }
+
+    selector_map = {
+        'title': '//meta[@property="og:title"]/@content',
+        'description': '//meta[@property="og:description"]/@content',
+        'date': '//p[contains(text(), "Added :")]/text()',
+        're_date': r'Added : (.*?)\s*-',
+        'image': '//meta[@property="og:image"]/@content',
+        'performers': '//p[contains(text(), "Starring")]/a/text()',
+        'tags': '//p[contains(text(), "Tags")]/a/text()',
+        'external_id': r'video=(\d+)',
+        'trailer': '',
+        'pagination': '/portal/more.php?aff=&page=%s&tag=&amatrice=&cat=&video=&search=&thid=&tr=&trlfap=&cp=&tunl=&iduser=&catalogue=&serie=&lang=en'
+    }
+
+    def get_scenes(self, response):
+        scenes = response.xpath('//a[contains(@href,"/video?")]/@href').getall()
+        for scene in scenes:
+            scene = html.unescape(scene)
+            scene = scene.replace("\\", " ").replace("\"", "").replace(" ", "")
+            if re.search(self.get_selector_map('external_id'), scene):
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+            else:
+                print(f'Didnt pull scene: {scene}')
+
+    def get_site(self, response):
+        return "La France a Poil"
+
+    def get_parent(self, response):
+        return "La France a Poil"
+
+    def get_next_page_url(self, base, page):
+        page = str(int(page) - 1)
+        return self.format_url(base, self.get_selector_map('pagination') % page)
+
+    def get_tags(self, response):
+        tags = super().get_tags(response)
+        if tags:
+            tags.append("European")
+        return tags
+
+    def get_title(self, response):
+        title = super().get_title(response)
+        title = unidecode(title)
+        return title
+
+    def get_description(self, response):
+        description = super().get_description(response)
+        description = unidecode(description)
+        return description
