6217735856d8900d11539a565457d262ea1c47b4
Weslocke <briadin@yahoo.com>
2021-09-13 18:24:50 -0400

    Initial commit of Mood Universe BDSM sites

---
+++ scenes/networkMoodUniverse.py
@@ -0,0 +1,123 @@
+import re
+import string
+import dateparser
+import tldextract
+import scrapy
+
+from tpdb.BaseSceneScraper import BaseSceneScraper
+from tpdb.items import SceneItem
+
+
+def match_site(argument):
+    match = {
+        'cruelamazons': "Cruel Amazons",
+        'cruel-ballbustings': "Cruel Ballbustings",
+        'cruel-handjobs': "Cruel Handjobs",
+        'cruel-strapon': "Cruel Strapon",
+        'domina-amanda': "Domina Amanda",
+        'ep-castings': "EP Castings",
+        'ep-cinema': "EP Cinema",
+        'lady-jenny': "Lady Jenny",
+        'mistress-ariel': "Mistress Ariel",
+        'mood-castings': "Mood Castings",
+        'mood-cinema': "Mood Cinema",
+        'russian-spanking': "Russian Spanking",
+    }
+    return match.get(argument, argument)
+
+
+def site_tags(argument):
+    match = {
+        'cruelamazons': ['Female Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain'],
+        'cruel-ballbustings': ['Female Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain', 'CBT'],
+        'cruel-handjobs': ['Female Domination', 'Bondage', 'BDSM', 'Handjob'],
+        'cruel-strapon': ['Female Domination', 'Bondage', 'BDSM', 'Pegging', 'Pain'],
+        'domina-amanda': ['Female Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain'],
+        'ep-castings': ['Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain'],
+        'ep-cinema': ['Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain'],
+        'lady-jenny': ['Female Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain', 'CBT'],
+        'mistress-ariel': ['Female Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain'],
+        'mood-castings': ['Domination', 'Bondage', 'BDSM', 'Spanking', 'Pain'],
+        'mood-cinema': ['Domination', 'Bondage', 'BDSM', 'Spanking', 'Pain'],
+        'russian-spanking': ['Domination', 'Bondage', 'BDSM', 'Spanking', 'Pain'],
+    }
+    return match.get(argument, argument)
+
+
+class NetworkMoodUniverseSpider(BaseSceneScraper):
+    name = 'MoodUniverse'
+    network = 'Mood Universe'
+
+    start_urls = [
+        'http://cruelamazons.com',
+        'http://cruel-ballbustings.com',
+        'http://cruel-handjobs.com',
+        'http://cruel-strapon.com',
+        'http://domina-amanda.com',
+        'http://archive.ep-castings.com',
+        'http://ep-castings.com',
+        'http://ep-cinema.com',
+        'http://lady-jenny.com',
+        'http://mistress-ariel.com',
+        'http://mood-castings.com',
+        'http://mood-cinema.com',
+        'http://russian-spanking.com',
+    ]
+
+    selector_map = {
+        'title': '',
+        'description': '',
+        'date': '',
+        'image': '',
+        'performers': '',
+        'tags': '',
+        'external_id': '',
+        'trailer': '',
+        'pagination': '/home.php?page=%s'
+    }
+
+    def get_scenes(self, response):
+        scenes = response.xpath('//div[contains(@class,"update")]')
+        for scene in scenes:
+            item = SceneItem()
+
+            title = scene.xpath('./div[@class="text"]/h2/text()')
+            if title:
+                item['title'] = string.capwords(title.get())
+            else:
+                item['title'] = ''
+
+            image = scene.xpath('.//a[contains(@class,"shot2")]/@href')
+            if image:
+                image = image.get()
+                image = re.sub(r'\?.*', '', image)
+                item['image'] = self.format_link(response, image)
+            else:
+                item['image'] = ''
+
+            item['url'] = response.url
+
+            scenedate = scene.xpath('.//span[@class="date"]/text()')
+            if scenedate:
+                scenedate = scenedate.get().strip()
+                item['date'] = dateparser.parse(scenedate, date_formats=['%d %b %Y']).isoformat()
+            else:
+                item['date'] = dateparser.parse('today').isoformat()
+
+            item['id'] = scene.xpath('./@id').get().strip()
+
+            item['tags'] = site_tags(tldextract.extract(response.url).domain)
+            item['performers'] = []
+            item['trailer'] = ''
+            item['description'] = ''
+
+            item['network'] = "Mood Universe"
+            if "archive" in response.url:
+                item['parent'] = "EP Castings Archive"
+                item['site'] = "EP Castings Archive"
+            else:
+                item['parent'] = match_site(tldextract.extract(response.url).domain)
+                item['site'] = match_site(tldextract.extract(response.url).domain)
+
+            if item['id'] and item['title']:
+                yield item
---
+++ scenes/networkMoodUniversePaged.py
@@ -0,0 +1,102 @@
+import re
+import string
+import dateparser
+import tldextract
+import scrapy
+
+from tpdb.BaseSceneScraper import BaseSceneScraper
+from tpdb.items import SceneItem
+
+
+def match_site(argument):
+    match = {
+        'cruel-mistresses': "Cruel Mistresses",
+        'elitepain': "Elite Pain",
+        'mood-pictures': "Mood Pictures",
+    }
+    return match.get(argument, argument)
+
+
+def site_tags(argument):
+    match = {
+        'cruel-mistresses': ['Female Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain'],
+        'mood-pictures': ['Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain'],
+        'elitepain': ['Domination', 'Bondage', 'BDSM', 'Whipping', 'Pain'],
+    }
+    return match.get(argument, argument)
+
+
+class NetworkMoodUniversePagedSpider(BaseSceneScraper):
+    name = 'MoodUniversePaged'
+    network = 'Mood Universe'
+
+    start_urls = [
+        'http://cruel-mistresses.com',
+        'http://elitepain.com',
+        'http://mood-pictures.com',
+    ]
+
+    def start_requests(self):
+        for link in self.start_urls:
+            link = link + "/movies.php"
+            yield scrapy.Request(link, callback=self.parse,
+                                 meta={'page': self.page},
+                                 headers=self.headers,
+                                 cookies=self.cookies)
+
+    selector_map = {
+        'title': '',
+        'description': '',
+        'date': '',
+        'image': '',
+        'performers': '',
+        'tags': '',
+        'external_id': '',
+        'trailer': '',
+        'pagination': '/home.php?page=%s'
+    }
+
+    def get_scenes(self, response):
+        if "cruel-mistresses" in response.url:
+            xpath = '//div[contains(@class,"movie")]'
+        else:
+            xpath = '//h2[contains(@id,"newestmovie") or contains(@id,"featuremovies") or contains(@id,"classics")]/following-sibling::div'
+
+
+        scenes = response.xpath(xpath)
+        for scene in scenes:
+            item = SceneItem()
+
+            title = scene.xpath('./h3/text()')
+            if title:
+                item['title'] = string.capwords(title.get())
+                item['title'] = re.sub(u'\u0096', u"\u0027", item['title'])
+                item['title'] = re.sub(u'\u0092', u"\u0027", item['title'])
+                item['title'] = item['title'].replace('`', '')
+            else:
+                item['title'] = ''
+
+            image = scene.xpath('.//img/@src')
+            if image:
+                image = image.get()
+                item['image'] = self.format_link(response, image)
+            else:
+                item['image'] = ''
+
+            item['url'] = self.format_link(response, scene.xpath('./a[1]/@href').get())
+
+            item['date'] = dateparser.parse('today').isoformat()
+
+            item['id'] = scene.xpath('./@id').get().strip()
+
+            item['tags'] = site_tags(tldextract.extract(response.url).domain)
+            item['performers'] = []
+            item['trailer'] = ''
+            item['description'] = ''
+
+            item['network'] = "Mood Universe"
+            item['parent'] = match_site(tldextract.extract(response.url).domain)
+            item['site'] = match_site(tldextract.extract(response.url).domain)
+
+            if item['id'] and item['title'] and not re.match(r'best\d{1,3}', item['id']):
+                yield item
