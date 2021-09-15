86d204a9eaf58edf29f94f223b973bcdfc6eff9d
Weslocke <briadin@yahoo.com>
2021-09-14 22:16:20 -0400

    Initial commit of Digital Desire and performers

---
+++ performers/siteDigitalDesirePerformer.py
@@ -0,0 +1,62 @@
+import html
+
+from tpdb.BasePerformerScraper import BasePerformerScraper
+from tpdb.items import PerformerItem
+
+
+class SiteDigitalDesireSpider(BasePerformerScraper):
+    selector_map = {
+        'name': '//div[@class="update_details"]/a[1]/text()',
+        'image': "//div[contains(@class,'image_area')]/img[@class='img-responsive']/@src",
+        'pagination': '/tour/models/models_%s_d.html',
+        'external_id': 'girls/(.+)/?$'
+    }
+
+    name = 'DigitalDesirePerformer'
+    network = "Digital Desire"
+
+    start_urls = [
+        'https://digitaldesire.com',
+    ]
+
+    def get_performers(self, response):
+        print(response.url)
+        performers = response.xpath('//div[@class="update_details"]')
+        for performer in performers:
+            item = PerformerItem()
+
+            name = performer.xpath('./a[contains(@href, "/models/")][1]/text()').get()
+            if name:
+                item['name'] = html.unescape(name.strip().title())
+
+            image = performer.xpath('.//a/img/@src0_1x').get()
+            print(image)
+            if image:
+                item['image'] = "https:" + image.replace(" ", "%20")
+            else:
+                item['image'] = ''
+
+            url = performer.xpath('./a[contains(@href, "/models/")][1]/@href').get()
+            if url:
+                item['url'] = self.format_link(response, url.strip()).replace(" ", "%20")
+
+            item['network'] = 'Digital Desire'
+
+            item['astrology'] = ''
+            item['bio'] = ''
+            item['birthday'] = ''
+            item['birthplace'] = ''
+            item['cupsize'] = ''
+            item['ethnicity'] = ''
+            item['eyecolor'] = ''
+            item['fakeboobs'] = ''
+            item['gender'] = 'Female'
+            item['haircolor'] = ''
+            item['height'] = ''
+            item['measurements'] = ''
+            item['nationality'] = ''
+            item['piercings'] = ''
+            item['tattoos'] = ''
+            item['weight'] = ''
+
+            yield item
