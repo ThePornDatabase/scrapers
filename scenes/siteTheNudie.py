5846e3c178a8f2a1420cda6dccc0486281c9a17a
Weslocke <briadin@yahoo.com>
2021-10-17 20:44:15 -0400

    Initial commit of The Nudie

---
+++ scenes/siteTheNudie.py
@@ -0,0 +1,42 @@
+import re
+import dateparser
+import scrapy
+
+from tpdb.BaseSceneScraper import BaseSceneScraper
+
+
+class SiteTheNudieSpider(BaseSceneScraper):
+    name = 'TheNudie'
+    network = 'The Nudie'
+
+    start_urls = [
+        'https://www.thenudie.com',
+    ]
+
+    selector_map = {
+        'title': '//h1/text()',
+        'description': '//div[contains(text(), "Description")]/following-sibling::div/text()',
+        'date': '',
+        'image': '//div[contains(@class,"w-full")]//@poster-url',
+        'performers': '//div[contains(text(), "Starring")]/following-sibling::div/span/div/text()',
+        'tags': '//div[contains(text(), "Categories")]/following-sibling::div/span/a/text()',
+        'external_id': r'.*/(.*?)$',
+        'trailer': '',
+        'pagination': '/scenes?page=%s'
+    }
+
+    def get_scenes(self, response):
+        scenes = response.xpath('//div[contains(@class,"w-full")]/a/@href').getall()
+        for scene in scenes:
+            scene = scene.strip()
+            if re.search(self.get_selector_map('external_id'), scene) and "signup" not in scene:
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+
+    def get_site(self, response):
+        return "The Nudie"
+
+    def get_parent(self, response):
+        return "The Nudie"
+
+    def get_date(self, response):
+        return dateparser.parse('today').isoformat()
