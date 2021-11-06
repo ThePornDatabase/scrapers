ae13ee42bcb505090a80cfe82656f41f4376ef6a
Weslocke <briadin@yahoo.com>
2021-11-06 10:17:03 -0400

    Small fix for date and formatting tweaks

--- scenes/networkWTFPass.py
+++ scenes/networkWTFPass.py
@@ -1,32 +1,30 @@
-import scrapy
 import re
-import tldextract
-from dateutil.relativedelta import relativedelta
-import dateparser
-import datetime
-import string
 import html
+import datetime
+import dateparser
+from dateutil.relativedelta import relativedelta
+import tldextract
+import scrapy
 
 from tpdb.BaseSceneScraper import BaseSceneScraper
 
 # ~ Network contains the following sites:
-     # ~ College Fuck Parties 
-     # ~ Dolls Porn 
-     # ~ Hard Fuck Girls 
-     # ~ HD Massage Porn 
-     # ~ Meet Suck And Fuck 
-     # ~ Panda Fuck 
-     # ~ Pickup Fuck 
-     # ~ Porn Weekends 
-     # ~ Private Sex Tapes 
-     # ~ Public Sex Adventures 
-     # ~ The Art Porn 
+# ~ College Fuck Parties
+# ~ Dolls Porn
+# ~ Hard Fuck Girls
+# ~ HD Massage Porn
+# ~ Meet Suck And Fuck
+# ~ Panda Fuck
+# ~ Pickup Fuck
+# ~ Porn Weekends
+# ~ Private Sex Tapes
+# ~ Public Sex Adventures
+# ~ The Art Porn
 
 
-class networkWTFPassSpider(BaseSceneScraper):
+class NetworkWTFPassSpider(BaseSceneScraper):
     name = 'WTFPass'
     network = 'WTF Pass'
-
 
     start_urls = [
         'https://wtfpass.com',
@@ -35,11 +33,11 @@ class networkWTFPassSpider(BaseSceneScraper):
     selector_map = {
         'title': '//meta[@property="og:title"]/@content',
         'description': '//meta[@name="description"]/@content',
-        'date': '',  #Site does not have valid dates, so it is calculated from xx Days/Months/Years ago
+        'date': '',  # Site does not have valid dates, so it is calculated from xx Days/Months/Years ago
         'image': '//meta[@property="og:image"]/@content',
         'performers': '//div[@class="data-row data-actress"]/a/text()',
         'tags': '//div[contains(@class,"tab-info")]/div[@class="data-row"]/a/text()',
-        'external_id': 'videos\/(\d+)\/',
+        'external_id': r'videos/(\d+)/',
         'trailer': '',
         'pagination': '/videos/%s/'
     }
@@ -48,7 +46,7 @@ class networkWTFPassSpider(BaseSceneScraper):
         meta = {}
         scenes = response.xpath('//div[@class="thumb-video cf"]')
         for scene in scenes:
-            
+
             meta['date'] = dateparser.parse('today').isoformat()
             date = scene.xpath('.//span[@class="date-added"]/text()')
             if date:
@@ -56,7 +54,9 @@ class networkWTFPassSpider(BaseSceneScraper):
                 date = parse_date(date)
                 if date:
                     meta['date'] = date
-            
+            else:
+                meta['date'] = dateparser.parse('today').isoformat()
+
             scene = scene.xpath('./div/a[contains(@href,"/videos/")]/@href').get()
             if re.search(self.get_selector_map('external_id'), scene):
                 yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene, meta=meta)
@@ -68,19 +68,15 @@ class networkWTFPassSpider(BaseSceneScraper):
 
         return tldextract.extract(response.url).domain
 
-
     def get_parent(self, response):
         return "WTF Pass"
-        
-
 
     def get_description(self, response):
         description = self.process_xpath(response, self.get_selector_map('description')).get()
         if description:
             if "Reality porn videos from hot college sex" in description:
                 return ''
-            else:
-                return html.unescape(description.strip())
+            return html.unescape(description.strip())
 
         return ''
 
@@ -88,14 +84,14 @@ class networkWTFPassSpider(BaseSceneScraper):
 def parse_date(datestring):
     today = datetime.datetime.now()
     datestring = datestring.lower()
-    intervalcount = re.search('(\d+)', datestring).group(1)
+    intervalcount = re.search(r'(\d+)', datestring).group(1)
     if not intervalcount:
         intervalcount = 0
     else:
         intervalcount = int(intervalcount)
-    if "minutes" in datestring:
+    if "minute" in datestring:
         date = today - relativedelta(minutes=intervalcount)
-    if "hours" in datestring:
+    if "hour" in datestring:
         date = today - relativedelta(hours=intervalcount)
     if "day" in datestring:
         date = today - relativedelta(days=intervalcount)
