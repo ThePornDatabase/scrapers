78dc4b858b854b88b04ddc017bf1e6c8e8cc3ae8
DirtyRacer1337 <DirtyRacer@ya.ru>
2021-11-12 07:39:35 +0700

    Update network5kPorn.py

--- scenes/network5kPorn.py
+++ scenes/network5kPorn.py
@@ -1,15 +1,7 @@
-import warnings
-import dateparser
 import scrapy
 from scrapy.http import HtmlResponse
 
 from tpdb.BaseSceneScraper import BaseSceneScraper
-
-# Ignore dateparser warnings regarding pytz
-warnings.filterwarnings(
-    "ignore",
-    message="The localize method is no longer necessary, as this time zone supports the fold attribute",
-)
 
 
 class FiveKPornSpider(BaseSceneScraper):
@@ -26,24 +18,16 @@ class FiveKPornSpider(BaseSceneScraper):
     selector_map = {
         'title': "//p[@class='trailer-title']/text()",
         'description': '//div[contains(@class, "video-summary")]//p[@class=""]/text()',
-        'date': "//div[@class='row video-summary']/div/h5[4]/text()",
+        'date': '//h5[contains(text(), "Published")]/text()',
         'image': '//div[contains(@class, "gal")]//img/@src',
         'performers': '//h5[contains(., "Starring")]/a/text()',
-        'tags': "",
-        'external_id': 'episodes\\/(.+)',
+        'tags': '',
+        'external_id': r'episodes/(.+)',
         'trailer': '',
         'pagination': '/episodes/search?page=%s'
     }
 
     def get_scenes(self, response):
-        rsp = HtmlResponse(url=response.url, body=response.json()[
-                           'html'], encoding='utf-8')
+        rsp = HtmlResponse(url=response.url, body=response.json()['html'], encoding='utf-8')
         for scene in rsp.css('.thumb-holder a::attr(href)').getall():
             yield scrapy.Request(url=scene, callback=self.parse_scene, cookies=self.cookies)
-
-    def get_date(self, response):
-        date = response.xpath('//h5[contains(text(), "Published")]/text()')
-        if date:
-            date = date.get()
-            return dateparser.parse(date.replace('Published:', '').strip()).isoformat()
-        return dateparser.parse('today').isoformat()
