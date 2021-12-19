f140bc26e5d6a6dfc4cfa210272d59a92f5ab67b
Weslocke <briadin@yahoo.com>
2021-12-19 11:06:18 -0500

    Adding GotFilled to scraper

--- scenes/siteBJRaw.py
+++ scenes/siteBJRaw.py
@@ -8,11 +8,10 @@ from tpdb.items import SceneItem
 
 class BJRawSpider(BaseSceneScraper):
     name = 'BJRaw'
-    network = 'BJ Raw'
-    parent = 'BJ Raw'
 
     start_urls = [
-        'https://www.bjraw.com'
+        'https://www.bjraw.com',
+        'https://www.gotfilled.com'
     ]
 
     selector_map = {
@@ -45,21 +44,29 @@ class BJRawSpider(BaseSceneScraper):
             item['trailer'] = jsonentry['trailer']['src']
             if item['trailer'] == "https://c2d8j4g8.ssl.hwcdn.net/6/0/2/5/8/60258852ed44c/bjr0005_rachaelcavalli _trailer.mp4":  # For some reason shows this scene trailer as invalid
                 item['trailer'] = ''
+            item['date'] = jsonentry['release_date']
             urltext = re.sub(r'[^A-Za-z0-9 ]+', '', jsonentry['title']).lower()
             urltext = urltext.replace("  ", " ")
             urltext = urltext.replace(" ", "-")
-            urltext = "https://www.bjraw.com/tour/videos/" + str(jsonentry['id']) + "/" + urltext
+
+            if 'bjraw' in response.url:
+                urltext = "https://www.bjraw.com/tour/videos/" + str(jsonentry['id']) + "/" + urltext
+                item['tags'] = ['Blowjob']
+                item['site'] = "BJ Raw"
+                item['parent'] = "BJ Raw"
+                item['network'] = "BJ Raw"
+            if 'gotfilled' in response.url:
+                urltext = "https://www.gotfilled.com/tour/videos/" + str(jsonentry['id']) + "/" + urltext
+                item['tags'] = ['Creampie']
+                item['site'] = "Got Filled"
+                item['parent'] = "Got Filled"
+                item['network'] = "Got Filled"
+
             item['url'] = urltext
-            item['date'] = jsonentry['release_date']
-            item['site'] = "BJ Raw"
-            item['parent'] = "BJ Raw"
-            item['network'] = "BJ Raw"
 
             item['performers'] = []
             for model in jsonentry['models']:
                 item['performers'].append(model['name'])
-
-            item['tags'] = []
 
             days = int(self.days)
             if days > 27375:
