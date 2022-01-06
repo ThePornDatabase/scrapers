4a659ccece939b502a5472b0fbfe9d59d56ebac8
Weslocke <briadin@yahoo.com>
2022-01-06 11:09:20 -0500

    Updating Virtual Taboo for new site design

--- scenes/siteVirtualTaboo.py
+++ scenes/siteVirtualTaboo.py
@@ -1,110 +1,84 @@
 import re
+import json
 from datetime import date, timedelta
+import scrapy
 from tpdb.BaseSceneScraper import BaseSceneScraper
 from tpdb.items import SceneItem
 
 
 class SiteVirtualTabooSpider(BaseSceneScraper):
     name = 'VirtualTaboo'
-    network = 'POVR'
-    parent = 'Virtual Taboo'
-    site = 'Virtual Taboo'
 
     start_urls = [
         'https://virtualtaboo.com',
     ]
 
     selector_map = {
-        'title': '//div[@class="title clear"]/h2/text()',
-        'description': '//span[contains(@class,"description")]/text()',
-        'date': '//span[contains(@class,"update_date")]/text()',
-        'image': '//span[@class="model_update_thumb"]/img/@src',
-        'performers': '//span[@class="tour_update_models"]/a/text()',
-        'tags': '//span[@class="update_tags"]/a/text()',
-        'external_id': r'updates/(.*).html',
+        'title': '',
+        'description': '',
+        'date': '',
+        'image': '',
+        'performers': '',
+        'tags': '',
+        'external_id': r'.*/(.*?)',
         'trailer': '',
         'pagination': '/videos?page=%s'
     }
 
     def get_scenes(self, response):
-        scenes = response.xpath('//div[@class="video-item"]')
+        scenes = response.xpath('//a[contains(@class, "video-card")]/@href').getall()
         for scene in scenes:
-            item = SceneItem()
+            if re.search(self.get_selector_map('external_id'), scene):
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
 
-            title = scene.xpath('.//div[contains(@class, "video-title")]/a/text()')
-            if title:
-                item['title'] = self.cleanup_title(title.get())
+    def parse_scene(self, response):
+        jsondata = response.xpath('//script[contains(@type, "json")]').get()
+        jsondata = re.search(r'(\{.*\})', jsondata).group(1)
+        jsondata = json.loads(jsondata)
+        item = SceneItem()
+
+        item['performers'] = []
+        for model in jsondata['video']['actor']:
+            item['performers'].append(model['name'].title())
+
+        item['title'] = self.cleanup_title(jsondata['video']['name'])
+        item['description'] = self.cleanup_description(jsondata['video']['description'])
+        if not item['description']:
+            item['description'] = ''
+
+        item['image'] = jsondata['video']['thumbnail']
+        if not item['image']:
+            item['image'] = None
+        item['image_blob'] = None
+        item['trailer'] = ''
+        item['url'] = jsondata['video']['url']
+        item['id'] = re.search(r'videos/(.*)', item['url']).group(1)
+        item['date'] = self.parse_date(jsondata['video']['datePublished'].strip()).isoformat()
+        item['site'] = "Virtual Taboo"
+        item['parent'] = "POVR"
+        item['network'] = "POVR"
+
+        item['tags'] = jsondata['video']['keywords']
+        tags2 = item['tags'].copy()
+        for tag in tags2:
+            if re.match(r'\d+K', tag):
+                item['tags'].remove(tag)
+        item['tags'] = list(map(lambda x: x.strip().title(), set(item['tags'])))
+
+        days = int(self.days)
+        if days > 27375:
+            filterdate = "0000-00-00"
+        else:
+            filterdate = date.today() - timedelta(days)
+            filterdate = filterdate.strftime('%Y-%m-%d')
+
+        if self.debug:
+            if not item['date'] > filterdate:
+                item['filtered'] = "Scene filtered due to date restraint"
+            print(item)
+        else:
+            if filterdate:
+                if item['date'] > filterdate:
+                    yield item
             else:
-                item['title'] = None
-
-            scenedate = scene.xpath('.//div[@class="info"]/span[@class="bullet"][2]/following-sibling::text()')
-            if scenedate:
-                scenedate = scenedate.get()
-                scenedate = re.search(r"(\d{1,2} \w+, \d{4})", scenedate)
-                if scenedate:
-                    item['date'] = self.parse_date(scenedate.group(1), date_formats=['%d %B, %Y']).isoformat()
-            else:
-                item['date'] = self.parse_date('today').isoformat()
-
-            performers = scene.xpath('.//div[@class="info"]/span[@class="bullet"][1]/following-sibling::a/text()')
-            if performers:
-                item['performers'] = list(map(lambda x: self.cleanup_title(x), performers.getall()))
-            else:
-                item['performers'] = []
-
-            tags = scene.xpath('.//div[@class="tag-list"]/a/text()')
-            if tags:
-                item['tags'] = list(map(lambda x: self.cleanup_title(x), tags.getall()))
-                words = ['4k', '5k', '6k', '7k', '8k']
-                for word in words:
-                    if word in item['tags']:
-                        item['tags'].remove(word)
-            else:
-                item['tags'] = ['VR Porn']
-
-            image = scene.xpath('./div[contains(@class, "images-container")]/div[contains(@class, "left-col")]/a/@data-lazy-bg')
-            if image:
-                image = image.get()
-                if "?p" in image:
-                    image = re.search(r'(.*?\.jpg)\?p', image).group(1)
-                    item['image'] = self.format_link(response, image)
-            else:
-                item['image'] = None
-
-            description = scene.xpath('.//div[@class="description"]/span/text()')
-            if description:
-                item['description'] = self.cleanup_description(description.get())
-            else:
-                item['description'] = ''
-
-            url = scene.xpath('./div[contains(@class, "images-container")]/div[contains(@class, "left-col")]/a/@href')
-            if url:
-                item['url'] = self.format_link(response, url.get())
-                item['id'] = re.search(r'.*/(.*?)$', item['url']).group(1)
-            else:
-                item['url'] = response.url
-                item['id'] = None
-
-            item['image_blob'] = None
-            item['trailer'] = None
-            item['network'] = 'POVR'
-            item['parent'] = 'POVR'
-            item['site'] = 'Virtual Taboo'
-            if item['title'] and item['id'] and item['date'] > '2021-02-13':
-                days = int(self.days)
-                if days > 27375:
-                    filterdate = "0000-00-00"
-                else:
-                    filterdate = date.today() - timedelta(days)
-                    filterdate = filterdate.strftime('%Y-%m-%d')
-
-                if self.debug:
-                    if not item['date'] > filterdate:
-                        item['filtered'] = "Scene filtered due to date restraint"
-                    print(item)
-                else:
-                    if filterdate:
-                        if item['date'] > filterdate:
-                            yield item
-                    else:
-                        yield item
+                yield item
