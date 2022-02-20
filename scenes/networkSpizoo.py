4d4099971f76ebd8a636e28099ba880c1cc0f5f6
Weslocke <briadin@yahoo.com>
2022-02-20 00:35:18 -0500

    Adding Mr Lucky Raw and cleaning some extraneous code

--- scenes/networkSpizoo.py
+++ scenes/networkSpizoo.py
@@ -4,87 +4,75 @@ import scrapy
 from tpdb.BaseSceneScraper import BaseSceneScraper
 
 
+def match_site(argument):
+    match = {
+        'firstclasspov': "First Class POV",
+        'mrluckypov': "Mr Lucky POV",
+        'mrluckyraw': "Mr Lucky Raw",
+        'rawattack': "Raw Attack",
+        'realsensual': "Real Sensual",
+        'spizoo': "Spizoo",
+    }
+    return match.get(argument, argument)
+
+
 class SpizooSpider(BaseSceneScraper):
     name = 'Spizoo'
     network = "Spizoo"
 
     start_urls = [
         'https://firstclasspov.com/',
-        'https://mrluckypov.com/',
-        'https://rawattack.com/',
-        'https://realsensual.com/',
-        'https://www.spizoo.com',
+        # ~ 'https://mrluckypov.com/',
+        'https://mrluckyraw.com/',
+        # ~ 'https://rawattack.com/',
+        # ~ 'https://realsensual.com/',
+        # ~ 'https://www.spizoo.com',
     ]
 
     selector_map = {
         'title': '/',
         'description': '',
         'date': "//p[@class='date']/text()",
-        'image': '',
-        'performers': '',
-        'tags': "",
+        'image': '//video/@poster|//div[@id="hpromo"]/a/img[contains(@src,".jpg")]/@src',
+        'performers': '//div[@class="col-12"]//a[contains(@href, "/models")]/@title|//div[@class="col-3"]//a[contains(@href, "/models")]/@title',
+        'tags': '//a[contains(@class,"category-tag")]/@title|//a[contains(@href,"/categories/")]/text()',
         'external_id': r'/updates/(.*)\.html$',
         'trailer': '',  # Hashed and tokened link.  Will be no good later
         'pagination': '/categories/movies_%s_d.html'
     }
 
     def get_scenes(self, response):
-        scenes = response.xpath("//a[@data-event='106']/@href").getall()
+        if "mrluckyraw" in response.url:
+            scenes = response.xpath("//div[@class='thumb-title']/a/@href").getall()
+        else:
+            scenes = response.xpath("//a[@data-event='106']/@href").getall()
         for scene in scenes:
             if re.search(self.get_selector_map('external_id'), scene):
                 yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
 
     def get_title(self, response):
-        if "spizoo" in response.request.url:
+        if "spizoo" in response.url or "mrluckyraw" in response.url:
             titlexpath = '//div[@class="title"]/h1/text()'
         matches = ["firstclasspov", "mrluckypov"]
-        if any(x in response.request.url for x in matches):
+        if any(x in response.url for x in matches):
             titlexpath = '//section[@id="scene"]/div/div/div/h1/text()'
-        if "rawattack" in response.request.url:
+        if "rawattack" in response.url:
             titlexpath = '//title/text()'
-        if "realsensual" in response.request.url:
+        if "realsensual" in response.url:
             titlexpath = '//h2[contains(@class,"titular")]/text()'
         return response.xpath(titlexpath).get().strip()
 
-    def get_tags(self, response):
-        if "spizoo" in response.request.url:
-            tagsxpath = '//a[contains(@class,"category-tag")]/@title'
-        else:
-            tagsxpath = '//a[contains(@href,"/categories/")]/text()'
-
-        tags = response.xpath(tagsxpath).getall()
-        return list(map(lambda x: x.strip(), tags))
-
-    def get_performers(self, response):
-        matches = ["firstclasspov", "rawattack"]
-        if any(x in response.request.url for x in matches):
-            performersxpath = '//div[@class="col-12"]//a[contains(@href, "/models")]/@title'
-        matches = ["mrluckypov", "realsensual", "spizoo"]
-        if any(x in response.request.url for x in matches):
-            performersxpath = '//div[@class="col-3"]//a[contains(@href, "/models")]/@title'
-
-        tags = response.xpath(performersxpath).getall()
-        return list(map(lambda x: x.strip(), tags))
-
-    def get_image(self, response):
-        if "rawattack" in response.request.url:
-            imagexpath = '//div[@id="hpromo"]/a/img[contains(@src,".jpg")]/@src'
-        else:
-            imagexpath = '//video/@poster'
-        return response.xpath(imagexpath).get().strip()
-
-    def get_trailer(self, response):
-        if "rawattack" in response.request.url:
-            trailerxpath = '//video[@id="trailervideo"]/source/@src'
-        else:
-            trailerxpath = '//video/source/@src'
-        return response.xpath(trailerxpath).get().strip()
-
     def get_description(self, response):
-        if "rawattack" in response.request.url:
+        if "rawattack" in response.url:
             descriptionxpath = '//section[@id="sceneInfo"]/div/div/div/p/text()'
-        elif "realsensual" in response.request.url:
+        elif "realsensual" in response.url:
             descriptionxpath = '//p[@class="description-scene"]/text()'
         else:
             descriptionxpath = '//p[@class="description"]/text()'
         return response.xpath(descriptionxpath).get().strip()
+
+    def get_site(self, response):
+        return match_site(super().get_site(response))
+
+    def get_parent(self, response):
+        return match_site(super().get_parent(response))
