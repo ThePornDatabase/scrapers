65afd31bbbbccdd03aeecd82b36e07f45d7527bf
Weslocke <briadin@yahoo.com>
2022-02-22 19:56:40 -0500

    Fixing images and trailers

--- scenes/siteRealGirlsGoneBad.py
+++ scenes/siteRealGirlsGoneBad.py
@@ -20,11 +20,11 @@ class SiteRealGirlsGoneBadSpider(BaseSceneScraper):
         'description': '//div[@class="eachB"]/p/text()',
         'date': '//div[@class="eachB"]//strong[contains(text(), "Added")]/following-sibling::text()',
         'date_formats': ['%d %B, %Y'],
-        'image': '//meta[@property="og:image"]/@content',
+        'image': '//script[contains(text(), "poster")]/text()',
         'performers': '',
         'tags': '//div[@class="eachB"]//span[@class="tagsC"]/a/text()',
         'external_id': r'.*/(.*?).html',
-        'trailer': '',
+        'trailer': '//script[contains(text(), "poster")]/text()',
         'pagination': '/tour/categories/videos_%s_d.html'
     }
 
@@ -33,3 +33,21 @@ class SiteRealGirlsGoneBadSpider(BaseSceneScraper):
         for scene in scenes:
             if re.search(self.get_selector_map('external_id'), scene):
                 yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene, cookies=self.cookies)
+
+    def get_image(self, response):
+        image = response.xpath(self.get_selector_map('image'))
+        if image:
+            image = image.get()
+            image = re.search(r'poster=\"(.*?.jpg)', image)
+            if image:
+                return "https://www.realgirlsgonebad.com" + image.group(1)
+        return ''
+
+    def get_trailer(self, response):
+        trailer = response.xpath(self.get_selector_map('trailer'))
+        if trailer:
+            trailer = trailer.get()
+            trailer = re.search(r'video src=\"(.*?.mp4)', trailer)
+            if trailer:
+                return "https://www.realgirlsgonebad.com" + trailer.group(1)
+        return ''
