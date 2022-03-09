053b58c7f30a34ec461399f8d013519e5bd2a42a
Weslocke <briadin@yahoo.com>
2022-03-08 21:29:28 -0500

    Fixes for dates

--- scenes/networkWTFPassAlt.py
+++ scenes/networkWTFPassAlt.py
@@ -1,4 +1,7 @@
 import re
+import datetime
+import dateparser
+from dateutil.relativedelta import relativedelta
 import tldextract
 import scrapy
 from tpdb.BaseSceneScraper import BaseSceneScraper
@@ -11,6 +14,7 @@ def match_site(argument):
         'hardfucktales': 'Hard Fuck Tales',
         'mypickupgirls': 'My Pickup Girls',
         'porntraveling': 'Porn Traveling',
+        'studentsexparties': 'Student Sex Parties',
     }
     return match.get(argument, argument)
 
@@ -25,14 +29,13 @@ class NetworkSeriousPartnersSpider(BaseSceneScraper):
         'https://hardfucktales.com',
         'https://mypickupgirls.com',
         'https://porntraveling.com',
+        'https://studentsexparties.com',
     ]
 
     selector_map = {
         'title': '//meta[@property="og:title"]/@content',
         'description': '//meta[@name="description"]/@content',
-        'date': '//script[contains(text(), "alltubes")]/text()',
-        're_date': r'jwLocationHost.*?js\?t(\d+)\.',
-        'date_formats': ['%Y%m%d'],
+        'date': '',
         'image': '//meta[@property="og:image"]/@content',
         'performers': '//div[contains(@class, "data-actress")]/a[@class="link-red model-link-overlay"]/text()',
         'tags': '//div[@class="video-page-tab-info"]//a[contains(@href, "categories")]/text()',
@@ -42,13 +45,53 @@ class NetworkSeriousPartnersSpider(BaseSceneScraper):
     }
 
     def get_scenes(self, response):
-        scenes = response.xpath('//div[@class="thumb-container"]/a[@class="thumb-video-link"]/@href').getall()
+        meta = {}
+        scenes = response.xpath('//div[@class="thumb-video cf"]')
         for scene in scenes:
+
+            meta['date'] = self.parse_date('today').isoformat()
+            date = scene.xpath('.//span[@class="date-added"]/text()')
+            if date:
+                date = date.get()
+                date = parse_date(date)
+                if date:
+                    meta['date'] = date
+            else:
+                meta['date'] = dateparser.parse('today').isoformat()
+
+            scene = scene.xpath('./div/a[contains(@href,"/videos/")]/@href').get()
             if re.search(self.get_selector_map('external_id'), scene):
-                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
+                yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene, meta=meta)
 
     def get_parent(self, response):
         return match_site(tldextract.extract(response.url).domain)
 
     def get_site(self, response):
         return match_site(tldextract.extract(response.url).domain)
+
+def parse_date(datestring):
+    today = datetime.datetime.now()
+    datestring = datestring.lower()
+    intervalcount = re.search(r'(\d+)', datestring).group(1)
+    if not intervalcount:
+        intervalcount = 0
+    else:
+        intervalcount = int(intervalcount)
+    if "minute" in datestring:
+        date = today - relativedelta(minutes=intervalcount)
+    if "hour" in datestring:
+        date = today - relativedelta(hours=intervalcount)
+    if "day" in datestring:
+        date = today - relativedelta(days=intervalcount)
+    if "today" in datestring:
+        date = today
+    if "yesterday" in datestring:
+        date = today - relativedelta(days=1)
+    if "week" in datestring:
+        date = today - relativedelta(weeks=intervalcount)
+    if "month" in datestring:
+        date = today - relativedelta(months=intervalcount)
+    if "year" in datestring:
+        date = today - relativedelta(years=intervalcount)
+
+    return date.isoformat()
