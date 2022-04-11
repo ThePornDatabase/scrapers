a473c45af491534d866e38ce400031bd70e3b175
Weslocke <briadin@yahoo.com>
2022-04-11 03:17:02 -0400

    Disabling pull for Abbie Maley from SexyHub.  Using FullPorn instead

--- scenes/siteAbbeyMaley.py
+++ scenes/siteAbbeyMaley.py
@@ -1,19 +1,21 @@
 import re
 import scrapy
-import tldextract
 from tpdb.BaseSceneScraper import BaseSceneScraper
 
-### Abbiemaley.com has all scenes hidden behind a paywall.
-### Sexyhub seems to have recent updates, and is getting current ones as 
-### well, so I'm pulling from there.
+# Abbiemaley.com has all scenes hidden behind a paywall.
+# Sexyhub seems to have recent updates, and is getting current ones as
+# well, so I'm pulling from there.
 
-class siteAbbieMaleySpider(BaseSceneScraper):
+# Update: Now pulling from FullPorn network
+
+
+class SiteAbbieMaleySpider(BaseSceneScraper):
     name = 'AbbieMaley'
     network = "Abbie Maley"
     parent = "Abbie Maley"
 
     start_urls = [
-        'https://www.sexyhub.org',
+        # ~ 'https://www.sexyhub.org',
     ]
 
     selector_map = {
@@ -24,7 +26,7 @@ class siteAbbieMaleySpider(BaseSceneScraper):
         'image': '//meta[@property="og:image"]/@content',
         'performers': '//div[@class="models"]/a/text()',
         'tags': '//div[contains(text(),"Categories")]/following-sibling::span/a/text()',
-        'external_id': '.*\/\d+-(.*)-abbiemaley',
+        'external_id': r'.*/\d+-(.*)-abbiemaley',
         'trailer': '',
         'pagination': '/xfsearch/site/AbbieMaley.com/page/%s/'
     }
@@ -34,7 +36,6 @@ class siteAbbieMaleySpider(BaseSceneScraper):
         for scene in scenes:
             if re.search(self.get_selector_map('external_id'), scene):
                 yield scrapy.Request(url=self.format_link(response, scene), callback=self.parse_scene)
-
 
     def get_performers(self, response):
         performers = self.process_xpath(response, self.get_selector_map('performers')).getall()
@@ -49,7 +50,6 @@ class siteAbbieMaleySpider(BaseSceneScraper):
             return list(map(lambda x: x.strip().title(), performerlist))
 
         return []
-
 
     def get_tags(self, response):
         if self.get_selector_map('tags'):
@@ -66,7 +66,6 @@ class siteAbbieMaleySpider(BaseSceneScraper):
             return list(map(lambda x: x.strip(), tags))
 
         return []
-        
-        
+
     def get_site(self, response):
         return "Abbie Maley"
