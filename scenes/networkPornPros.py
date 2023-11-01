6674fc12b935968019488bff1f6b07322d663072
deusofgamers <deusofgamers@gmail.com>
2023-11-01 14:36:06 -0400

    Updated with latest layout change

--- scenes/networkPornPros.py
+++ scenes/networkPornPros.py
@@ -1,6 +1,7 @@
 import re
 import dateparser
 import scrapy
+from slugify import slugify
 from tpdb.BaseSceneScraper import BaseSceneScraper
 
 
@@ -9,33 +10,33 @@ class PornprosSpider(BaseSceneScraper):
     network = 'pornpros'
 
     start_urls = [
-        "https://anal4k.com/",
-        "https://baeb.com/",
-        "https://bbcpie.com/",
-        "https://castingcouch-x.com/",
-        "https://cum4k.com/",
-        "https://exotic4k.com/",
-        "https://facials4k.com/",
-        "https://fantasyhd.com/",
-        "https://girlcum.com/",
-        "https://holed.com/",
-        "https://lubed.com/",
-        "https://myveryfirsttime.com/",
-        "https://nannyspy.com/",
-        "https://passion-hd.com/",
+        # "https://anal4k.com/",
+        # "https://baeb.com/",
+        # "https://bbcpie.com/",
+        # "https://castingcouch-x.com/",
+        # "https://cum4k.com/",
+        # "https://exotic4k.com/",
+        # "https://facials4k.com/",
+        # "https://fantasyhd.com/",
+        # "https://girlcum.com/",
+        # "https://holed.com/",
+        # "https://lubed.com/",
+        # "https://myveryfirsttime.com/",
+        # "https://nannyspy.com/",
+        # "https://passion-hd.com/",
         "https://pornpros.com/",
-        "https://povd.com/",
-        "https://puremature.com/",
-        "https://spyfam.com/",
-        'https://tiny4k.com/',
-        'https://wetvr.com/',
+        # "https://povd.com/",
+        # "https://puremature.com/",
+        # "https://spyfam.com/",
+        # 'https://tiny4k.com/',
+        # 'https://wetvr.com/',
     ]
 
     selector_map = {
-        'title': '//h1[@class="sceneTitle"]/text() | //h3[@class="dvdTitle"]/text() | //h1[contains(@class, "t2019-stitle")]/text()',
-        'description': '//div[@class="sceneDesc bioToRight showMore"]/text() | //div[@class="sceneDescText"]/text() | //p[@class="sceneDesc showMore"]/text() | //p[@class="descriptionText"]/text() | //div[@id="t2019-description"]/text()',
+        'title': '//h1[@class="sceneTitle"]/text() | //h3[@class="dvdTitle"]/text() | //h1[contains(@class, "t2019-stitle")]/text() | //div[contains(@class, "scene-info")]//h1/text()',
+        'description': '//div[@class="sceneDesc bioToRight showMore"]/text() | //div[@class="sceneDescText"]/text() | //p[@class="sceneDesc showMore"]/text() | //p[@class="descriptionText"]/text() | //div[@id="t2019-description"]/text() | //i[contains(@class, "fa-quote-right")]/../span/text()',
         'date': '//div[@class="tlcSpecs"]/span[@class="tlcSpecsDate"]/span[@class="tlcDetailsValue"]/text() | //*[@class="updatedDate"]/text() | //div[@id="t2019-stime"]//span/text()',
-        'performers': '//div[@class="sceneCol sceneColActors"]//a/text() | //div[@class="sceneCol scenePerformers"]//a/text() | //div[@class="pornstarName"]/text() | //div[@id="slick_DVDInfoPerformerCarousel"]//a/text() | //div[@id="slick_sceneInfoPlayerPerformerCarousel"]//a/text() | //div[@id="t2019-models"]/a/text()',
+        'performers': '//div[@class="sceneCol sceneColActors"]//a/text() | //div[@class="sceneCol scenePerformers"]//a/text() | //div[@class="pornstarName"]/text() | //div[@id="slick_DVDInfoPerformerCarousel"]//a/text() | //div[@id="slick_sceneInfoPlayerPerformerCarousel"]//a/text() | //div[@id="t2019-models"]/a/text() | //span[contains(text(),"Featuring")]/../a/text()',
         'tags': '//div[@class="sceneCol sceneColCategories"]//a/text() | //div[@class="sceneCategories"]//a/text() | //p[@class="dvdCol"]/a/text()',
         'duration': '//div[contains(@id, "stime")]/div/span[contains(text(), "minutes")]/text()',
         're_duration': r'(\d+)',
@@ -65,14 +66,24 @@ class PornprosSpider(BaseSceneScraper):
                                      cookies=self.cookies)
 
     def get_scenes(self, response):
-        scenes = response.xpath("//div[contains(@class, 'video-releases-list')]//div[@data-video-id]")
+        scenes = response.xpath("//div[contains(@class, 'items-center')]//nav/..//div[@data-vid]")
+
+        # In case a site is added that uses the old layout
+        if len(scenes) == 0:
+            scenes = response.xpath("//div[contains(@class, 'video-releases-list')]//div[@data-video-id]")
+
         for scene in scenes:
             link = self.format_link(response, scene.css('a::attr(href)').get())
+
+            # if link = /join we can guestimate by using the title and slugifying
+            if link == self.format_link(response, "/join"):
+                link = self.format_link(response, "/video/" + slugify(scene.xpath(".//a[contains(@class, 'title')]/text()").get()))
+
             meta = {}
 
             parsescene = True
             if "pornpros" in response.url:
-                link = self.format_link(response, scene.xpath('.//div[@class="information"]/a/@href').get())
+                link = self.format_link(response, scene.xpath('.//a[contains(@class,"title")]/@href').get())
                 sceneid = re.search(r'video/(.+)', link).group(1).strip()
                 with open('dupelist-pornpros.txt', 'r', encoding="utf-8") as file1:
                     for i in file1.readlines():
@@ -83,6 +94,9 @@ class PornprosSpider(BaseSceneScraper):
             if scene.css('div::attr(data-date)').get() is not None:
                 meta['date'] = dateparser.parse(
                     scene.css('div::attr(data-date)').get()).isoformat()
+            elif scene.xpath('.//div[contains(@class, "video-thumbnail-footer")]//span[contains(@class, "text-xs")]/text()').get() is not None:
+                meta['date'] = dateparser.parse(
+                    scene.xpath('.//div[contains(@class, "video-thumbnail-footer")]//span[contains(@class, "text-xs")]/text()').get()).isoformat()
 
             if parsescene:
                 yield scrapy.Request(url=self.format_link(response, link), callback=self.parse_scene, meta=meta)
