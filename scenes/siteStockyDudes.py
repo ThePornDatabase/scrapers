5988a628cf377c4a37c1f7b9f900182df437aef7
pbtcameron <pbtcameron@gmail.com>
2025-01-09 17:30:02 +0000

    Enhance SiteStockyDudesSpider and SiteStockyDudesPerformerSpider: update XPath selectors, improve pagination handling, and refactor scene extraction logic

--- scenes/siteStockyDudes.py
+++ scenes/siteStockyDudes.py
@@ -12,37 +12,38 @@ class SiteStockyDudesSpider(BaseSceneScraper):
 
     selector_map = {
         'title': '//h2[contains(@class,"sectionMainTitle")]/text()',
-        'description': '//div[@class="row container_styled_1"]//div[@class="p-5"]/p/text()',
-        'date': '//div[@class="row container_styled_1"]//div[@class="p-5"]//i[@class="icon-clock-1"]/preceding-sibling::text()',
+        'description': '//div[@class="row container_styled_1"]'
+        '//div[@class="p-5"]/p/text()',
+        'date': '//div[@class="row container_styled_1"]//div[@class="p-5"]'
+        '//i[@class="icon-clock-1"]/preceding-sibling::text()',
         're_date': r'([\w]*\s?\d{1,2}[,]?\s?\d{4})',
         'image': './/div[@id="playerWrap"]/img/@src',
-        'performers': '//span[@class="perfImage"]//br/following-sibling::text()',
+        'performers': '//span[@class="perfImage"]//br'
+        '/following-sibling::text()',
         'tags': '//a[@class="tagsRnd"]/text()',
-        'duration': '//div[@class="row container_styled_1"]//div[@class="p-5"]//i[@class="icon-clock-1"]/following-sibling::text()',
+        'duration': '//div[@class="row container_styled_1"]//div[@class="p-5"]'
+        '//i[@class="icon-clock-1"]/following-sibling::text()',
         'external_id': r'.*/(.*?)/?$',
         'pagination': '/scenes?Page=%s',
         'type': 'Scene',
     }
 
     def get_scenes(self, response):
-        yield from self.extract_scenes(response, response.selector)
+        selector = response.selector
 
-    def get_scenes_json(self, response):
-        html = response.json()['html']
+        if ('json' in response.headers.get('Content-Type').decode('utf-8')
+           and 'html' in response.json()):
+            selector = scrapy.Selector(text=response.json()['html'])
 
-        selector = scrapy.Selector(text=html)
-
-        yield from self.extract_scenes(response, selector)
-     
-    def extract_scenes(self, response, selector):
         scenes = selector.xpath(
-            '//div[@class="scene_title"]//a[contains(@href,"/scene/")]/@href'
-        ).getall()
+                '//div[@class="scene_title"]//a[contains(@href,"/scene/")]'
+                '/@href'
+            ).getall()
 
         for scene in scenes:
 
-            trailer = selector.xpath('//figure//a[@href="' + str(scene) +
-                                     '"]//source/@src').get()
+            trailer = selector.xpath('//figure//a[@href="' +
+                                     str(scene) + '"]//source/@src').get()
             meta = {}
             meta['trailer'] = trailer
 
@@ -56,7 +57,7 @@ class SiteStockyDudesSpider(BaseSceneScraper):
             tags.append("Homosexual")
 
         return tags
-        
+
     def get_pagin_data(self, response):
         page_data = {}
         page_data['from'] = int(response.xpath(
@@ -68,15 +69,13 @@ class SiteStockyDudesSpider(BaseSceneScraper):
         page_data['_'] = '1212121'
 
         return page_data
-        
+    
     def parse(self, response, **kwargs):
+        scenes = self.get_scenes(response)
         count = 0
 
         if 'pagingData' not in response.meta:
             response.meta['pagingData'] = self.get_pagin_data(response)
-            scenes = self.get_scenes(response)
-        else:
-            scenes = self.get_scenes_json(response)
 
         for scene in scenes:
             count += 1
@@ -87,14 +86,12 @@ class SiteStockyDudesSpider(BaseSceneScraper):
                     response.meta['page'] < self.limit_pages):
                 meta = response.meta
                 meta['page'] = meta['page'] + 1
-
                 meta['pagingData']['from'] += count
 
                 print('NEXT PAGE: ' + str(meta['page']))
 
-                link = self.format_link(response, '/_ajaxLoadScenes.php?' +
-                                        urlencode(meta['pagingData']))
-
+                link = self.format_link(response, '/_ajaxLoadScenes.php?'
+                                        + urlencode(meta['pagingData']))
                 self.headers['x-requested-with'] = 'XMLHttpRequest'
 
                 yield scrapy.Request(url=link, callback=self.parse, meta=meta,
