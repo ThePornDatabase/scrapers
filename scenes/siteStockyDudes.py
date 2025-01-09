ca1ebabfc59ade5f5710ec736d36aed758dbd0c6
pbtcameron <pbtcameron@gmail.com>
2025-01-09 03:40:41 +0000

    Fix for Stocky Dudes scraper

--- scenes/siteStockyDudes.py
+++ scenes/siteStockyDudes.py
@@ -1,6 +1,7 @@
-import re
 from tpdb.BaseSceneScraper import BaseSceneScraper
-from tpdb.items import SceneItem
+from urllib.parse import urlencode
+import scrapy
+
 
 
 class SiteStockyDudesSpider(BaseSceneScraper):
@@ -11,40 +12,103 @@ class SiteStockyDudesSpider(BaseSceneScraper):
     ]
 
     selector_map = {
-        'title': './/div[@class="sceneTitle"]/text()',
-        'description': './/div[@class="sceneDescription"]/text()',
-        'date': '',
-        'image': './/div[@class="bgScene"]/img/@src',
-        'performers': './/span[@class="perfName"]/text()',
-        'tags': './/span[@class="sceneTagsLnk"]/a/text()',
-        'duration': '',
-        'trailer': '///script[contains(text(), "jwplayer")]/text()',
-        're_trailer': r'(http.*?\.mp4)',
-        'external_id': r'',
+        'title': '//h2[contains(@class,"sectionMainTitle")]/text()',
+        'description': '//div[@class="row container_styled_1"]//div[@class="p-5"]/p/text()',
+        'date': '//div[@class="row container_styled_1"]//div[@class="p-5"]//i[@class="icon-clock-1"]/preceding-sibling::text()',
+        're_date': r'([\w]*\s?\d{1,2}[,]?\s?\d{4})',
+        'image': './/div[@id="playerWrap"]/img/@src',
+        'performers': '//span[@class="perfImage"]//br/following-sibling::text()',
+        'tags': '//a[@class="tagsRnd"]/text()',
+        'duration': '//div[@class="row container_styled_1"]//div[@class="p-5"]//i[@class="icon-clock-1"]/following-sibling::text()',
+        'external_id': r'.*/(.*?)/?$',
         'pagination': '/scenes?Page=%s',
-        'type': 'Scene',
+        'type': 'Scene', 
     }
 
     def get_scenes(self, response):
-        scenes = response.xpath('//div[@class="eachScene"]')
-        for scene in scenes:
-            item = SceneItem()
 
-            item['title'] = self.get_title(scene)
-            item['date'] = self.get_date(scene)
-            item['description'] = self.get_description(scene)
-            item['image'] = self.get_image(scene, response.url)
-            item['image_blob'] = self.get_image_blob_from_link(item['image'])
-            item['performers'] = self.get_performers(scene)
-            item['tags'] = self.get_tags(scene)
-            if "Gay" not in item['tags']:
-                item['tags'].append("Homosexual")
-            item['trailer'] = self.get_trailer(scene, response.url)
-            item['type'] = 'Scene'
-            item['duration'] = None
-            item['url'] = response.url
-            item['id'] = re.search(r'(\d+)', scene.xpath('./@id').get()).group(1)
-            item['site'] = "Stocky Dudes"
-            item['parent'] = "Stocky Dudes"
-            item['network'] = "Stocky Dudes"
-            yield item
+        yield from self.extract_scenes(response,response.selector)
+
+
+    def get_scenes_json(self, response):
+        
+        html =  response.json()['html']
+
+        selector = scrapy.Selector(text=html)
+
+        yield from self.extract_scenes(response,selector)
+
+        
+    def extract_scenes(self, response, selector):
+
+        scenes = selector.xpath('//div[@class="scene_title"]//a[contains(@href,"/scene/")]/@href').getall()
+
+        for scene in scenes:
+
+            trailer = selector.xpath(f'//figure//a[@href="{str(scene)}"]//source/@src').get()
+            meta = {}
+            meta['trailer'] = trailer
+
+            yield scrapy.Request(url=self.format_link(response,str(scene)), callback=self.parse_scene,meta=meta)
+
+
+    def get_tags(self, response):
+        tags = super().get_tags(response)
+
+        if "Gay" not in tags:
+            tags.append("Homosexual")
+
+        return tags
+    
+    def get_next_page(self, response):
+        if 'page' in response.meta:
+            return response.meta['page']
+        else:
+            return 1
+        
+    def get_pagin_data(self, response):
+        page_data = {}
+        page_data['from'] = response.xpath('//div[@id="scenesLoadMore"]/@data-from').get()
+        page_data['filter'] = response.xpath('//div[@id="scenesLoadMore"]/@data-filter').get()
+        page_data['sort'] = response.xpath('//div[@id="scenesLoadMore"]/@data-sort').get()
+        page_data['_'] = '1212121'
+
+        return page_data
+    
+    def parse(self, response, **kwargs):
+        scenes = self.get_scenes(response)
+        count = 0
+
+        if 'pagingData' not in response.meta:
+            response.meta['pagingData'] = self.get_pagin_data(response)
+            scenes = self.get_scenes(response)
+        else:
+            scenes = self.get_scenes_json(response)
+
+        
+        if 'count' not in response.meta:
+            response.meta['count'] = 0
+
+        
+
+        for scene in scenes:
+            count += 1
+            yield scene
+
+        if count:
+            if 'page' in response.meta and response.meta['page'] < self.limit_pages:
+                 
+                meta = response.meta
+                meta['page'] = meta['page'] + 1
+
+                meta['count'] += count
+
+                meta['pagingData']['from'] = str(meta['count'])
+
+                print('NEXT PAGE: ' + str(meta['page']))
+
+                link =  self.format_link(response,'/_ajaxLoadScenes.php?' + urlencode(meta['pagingData'])) 
+
+                self.headers['x-requested-with'] = 'XMLHttpRequest'
+
+                yield scrapy.Request(url=link, callback=self.parse, meta=meta, headers=self.headers, cookies=self.cookies)
